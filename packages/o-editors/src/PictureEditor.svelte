2d8fdd98a1e4f8fefb74e331049e3bfc000f9ba8
Thorsten Schau <letsgo@codeho.net>
2021-04-24 11:03:59 +0200

    fix double opening of file diaglog

--- packages/o-editors/src/PictureEditor.svelte
+++ packages/o-editors/src/PictureEditor.svelte
@@ -190,12 +190,6 @@
               class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
             >
               <span>Upload a file</span>
-              <input
-                id="file-upload"
-                name="file-upload"
-                type="file"
-                class="sr-only"
-              />
             </label>
             <p class="pl-1">or drag and drop</p>
           </div>
