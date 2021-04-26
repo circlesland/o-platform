0ef6644b2ff5716c1bb23b6ad384c2929f81f909
Thorsten Schau <letsgo@codeho.net>
2021-04-26 14:03:52 +0200

    dropdown Search

--- packages/o-editors/src/DropdownSelectItem.svelte
+++ packages/o-editors/src/DropdownSelectItem.svelte
@@ -1,25 +1,5 @@
 <script>
   export let item = undefined;
-  export let isActive = false;
-  export let isFirst = false;
-  export let isHover = false;
-
-  let itemClasses = "";
-
-  $: {
-    const classes = [];
-
-    if (isActive) {
-      classes.push("active");
-    }
-    if (isFirst) {
-      classes.push("first");
-    }
-    if (isHover) {
-      classes.push("hover");
-    }
-    itemClasses = classes.join(" ");
-  }
 </script>
 
 <section class="flex mb-2 items-centerjustify-center  text-circlesdarkblue">
@@ -49,52 +29,3 @@
     </div>
   </div>
 </section>
-
-<style>
-  .customItem {
-    display: flex;
-    align-items: center;
-    cursor: default;
-    height: 100px;
-    line-height: 40px;
-    padding: 0 16px;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    white-space: nowrap;
-  }
-
-  .customItem:active {
-    background: #b9daff;
-  }
-
-  .customItem.active {
-    background: #007aff;
-    color: #fff;
-  }
-
-  .customItem.first {
-    border-radius: 4px 4px 0 0;
-  }
-
-  .customItem.hover:not(.active) {
-    background: #e7f2ff;
-  }
-
-  .customItem_title {
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-
-  .customItem_name {
-    display: inline-block;
-    font-weight: 700;
-    margin-right: 10px;
-  }
-
-  .customItem_tagline {
-    display: inline-block;
-  }
-</style>
