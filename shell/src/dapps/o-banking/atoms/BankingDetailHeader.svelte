7548a9b1d4cb0dccf614e3090c23d53962ac359c
Thorsten Schau <letsgo@codeho.net>
2021-04-28 01:58:28 +0200

    #9 banking header

--- shell/src/dapps/o-banking/atoms/BankingDetailHeader.svelte
+++ shell/src/dapps/o-banking/atoms/BankingDetailHeader.svelte
@@ -24,29 +24,27 @@
     <div
       class="h-24 pt-0  flex flex-row  justify-between navbar bg-gradient-to-r from-gradient1 to-gradient2 text-white"
     >
-      <div class="self-start">
-        <a href="/#" on:click|once={() => history.back()}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 inline-block -mt-1 mr-1"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M10 19l-7-7m0 0l7-7m-7 7h18"
-            />
-          </svg>
-          <span class="text-lg font-circles">
-            {#if lastLoadedDapp.title != lastLoadedPage.title}
-              {lastLoadedDapp.title} /
-            {/if}
-            {lastLoadedPage.title}
-          </span>
-        </a>
+      <div class="self-start" on:click={() => history.back()}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 inline-block -mt-1 mr-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 19l-7-7m0 0l7-7m-7 7h18"
+          />
+        </svg>
+        <span class="text-lg font-circles">
+          {#if lastLoadedDapp.title != lastLoadedPage.title}
+            {lastLoadedDapp.title} /
+          {/if}
+          {lastLoadedPage.title}
+        </span>
       </div>
       <div class="self-start">
         <button
