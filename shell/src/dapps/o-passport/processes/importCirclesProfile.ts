388c5092a552874d9e08f7721bf4a9cb218e6204
Thorsten Schau <letsgo@codeho.net>
2021-04-27 14:53:07 +0200

    change placeholder

--- shell/src/dapps/o-passport/processes/importCirclesProfile.ts
+++ shell/src/dapps/o-passport/processes/importCirclesProfile.ts
@@ -1,28 +1,28 @@
-import {ProcessDefinition} from "@o-platform/o-process/dist/interfaces/processManifest";
-import {ProcessContext} from "@o-platform/o-process/dist/interfaces/processContext";
+import { ProcessDefinition } from "@o-platform/o-process/dist/interfaces/processManifest";
+import { ProcessContext } from "@o-platform/o-process/dist/interfaces/processContext";
 import TextEditor from "@o-platform/o-editors/src/TextEditor.svelte";
-import {prompt} from "@o-platform/o-process/dist/states/prompt";
-import {fatalError} from "@o-platform/o-process/dist/states/fatalError";
-import {createMachine} from "xstate";
-import {GnosisSafeProxy} from "@o-platform/o-circles/dist/safe/gnosisSafeProxy";
+import { prompt } from "@o-platform/o-process/dist/states/prompt";
+import { fatalError } from "@o-platform/o-process/dist/states/fatalError";
+import { createMachine } from "xstate";
+import { GnosisSafeProxy } from "@o-platform/o-circles/dist/safe/gnosisSafeProxy";
 import Web3 from "web3";
-import {RpcGateway} from "@o-platform/o-circles/dist/rpcGateway";
-import * as bip39 from "bip39"
+import { RpcGateway } from "@o-platform/o-circles/dist/rpcGateway";
+import * as bip39 from "bip39";
 
 export type ImportedCirclesProfileData = {
-  circlesAddress?: string
-  firstName?: string
-  avatarUrl?: string
-  privateKey?: string
-  accountAddress?: string
-}
+  circlesAddress?: string;
+  firstName?: string;
+  avatarUrl?: string;
+  privateKey?: string;
+  accountAddress?: string;
+};
 
 export type ImportCirclesProfileContextData = {
   safeAddress?: string;
-  seedPhrase?: string
+  seedPhrase?: string;
   safeOwners?: string[];
   circlesGardenProfile?: any;
-  profileData?: ImportedCirclesProfileData
+  profileData?: ImportedCirclesProfileData;
 };
 
 /**
@@ -37,9 +37,9 @@ export type ImportCirclesProfileContext = ProcessContext<ImportCirclesProfileCon
  */
 const strings = {
   labelSafeAddress: "Please enter your circles safe address",
-  placeholderSafeAddress: "you@example.com",
+  placeholderSafeAddress: "your safe address",
   labelSeedPhrase: "Please enter your seed phrase",
-  placeholderSeedPhrase: "Seedphrase"
+  placeholderSeedPhrase: "Seedphrase",
 };
 
 const processDefinition = (processId: string) =>
@@ -57,7 +57,7 @@ const processDefinition = (processId: string) =>
         params: {
           label: strings.labelSafeAddress,
           placeholder: strings.placeholderSafeAddress,
-          submitButtonText: "Connect"
+          submitButtonText: "Connect",
         },
         navigation: {
           next: "#checkSafeAddress",
@@ -70,27 +70,42 @@ const processDefinition = (processId: string) =>
             context.messages["safeAddress"] = ``;
             context.data.safeAddress = context.data.safeAddress?.trim();
             try {
-              console.log(`Checking if safe ${context.data.safeAddress} exists ..`);
+              console.log(
+                `Checking if safe ${context.data.safeAddress} exists ..`
+              );
               const web3 = await RpcGateway.get();
-              const safeProxy = new GnosisSafeProxy(web3, "", context.data.safeAddress);
+              const safeProxy = new GnosisSafeProxy(
+                web3,
+                "",
+                context.data.safeAddress
+              );
               context.data.safeOwners = await safeProxy.getOwners();
-              console.log(`Checking if safe ${context.data.safeAddress} exists .. Safe exists.`);
+              console.log(
+                `Checking if safe ${context.data.safeAddress} exists .. Safe exists.`
+              );
               return true;
             } catch (e) {
-              context.messages["safeAddress"] = `Couldn't determine the owner of safe ${context.data.safeAddress}. Is the address right?`;
-              console.log(`Checking if safe ${context.data.safeAddress} exists .. Safe doesn't exist.`, e);
+              context.messages[
+                "safeAddress"
+              ] = `Couldn't determine the owner of safe ${context.data.safeAddress}. Is the address right?`;
+              console.log(
+                `Checking if safe ${context.data.safeAddress} exists .. Safe doesn't exist.`,
+                e
+              );
               throw e;
             }
           },
           onDone: "#checkCirclesGarden",
-          onError: "#safeAddress"
-        }
+          onError: "#safeAddress",
+        },
       },
       checkCirclesGarden: {
         id: "checkCirclesGarden",
         invoke: {
           src: async (context) => {
-            const checksumAddress = Web3.utils.toChecksumAddress(context.data.safeAddress);
+            const checksumAddress = Web3.utils.toChecksumAddress(
+              context.data.safeAddress
+            );
             const queryUrl = `https://api.circles.garden/api/users/?address[]=${checksumAddress}`;
             const result = await fetch(queryUrl);
 
@@ -99,18 +114,21 @@ const processDefinition = (processId: string) =>
             }
 
             const resultJson = await result.json();
-            if (!Array.isArray(resultJson.data) || resultJson.data.length == 0) {
+            if (
+              !Array.isArray(resultJson.data) ||
+              resultJson.data.length == 0
+            ) {
               return;
             }
 
             context.data.circlesGardenProfile = {
               username: resultJson.data[0].username,
-              avatarUrl: resultJson.data[0].avatarUrl
+              avatarUrl: resultJson.data[0].avatarUrl,
             };
           },
           onDone: "#seedPhrase",
-          onError: "#error"
-        }
+          onError: "#error",
+        },
       },
       seedPhrase: prompt<ImportCirclesProfileContext, any>({
         fieldName: "seedPhrase",
@@ -137,26 +155,37 @@ const processDefinition = (processId: string) =>
               // TODO: 0x123 is for testing without private key. Needs to be removed later.
               keyFromMnemonic = "0x123";
               account = {
-                address: "0x123"
+                address: "0x123",
               };
             } else {
               try {
-                keyFromMnemonic = "0x" + bip39.mnemonicToEntropy(context.data.seedPhrase);
+                keyFromMnemonic =
+                  "0x" + bip39.mnemonicToEntropy(context.data.seedPhrase);
               } catch (e) {
-                context.messages["seedPhrase"] = `The seedphrase cannot be converted to a private key. Please double check it.`;
+                context.messages[
+                  "seedPhrase"
+                ] = `The seedphrase cannot be converted to a private key. Please double check it.`;
                 throw e;
               }
 
               try {
-                account = RpcGateway.get().eth.accounts.privateKeyToAccount(keyFromMnemonic);
+                account = RpcGateway.get().eth.accounts.privateKeyToAccount(
+                  keyFromMnemonic
+                );
               } catch (e) {
-                context.messages["seedPhrase"] = `The key that was generated from the seedphrase cannot be converted to an ethereum account.`;
+                context.messages[
+                  "seedPhrase"
+                ] = `The key that was generated from the seedphrase cannot be converted to an ethereum account.`;
                 throw e;
               }
 
-              if (!context.data.safeOwners.find(o => o === account.address)) {
-                context.messages["seedPhrase"] = `The given key doesn't belong to a owner of safe ${context.data.safeAddress}`;
-                throw new Error(`The given key doesn't belong to a owner of safe ${context.data.safeAddress}`)
+              if (!context.data.safeOwners.find((o) => o === account.address)) {
+                context.messages[
+                  "seedPhrase"
+                ] = `The given key doesn't belong to a owner of safe ${context.data.safeAddress}`;
+                throw new Error(
+                  `The given key doesn't belong to a owner of safe ${context.data.safeAddress}`
+                );
               }
 
               localStorage.setItem("circlesAccount", account.address);
@@ -166,7 +195,7 @@ const processDefinition = (processId: string) =>
               accountAddress: account.address,
               circlesAddress: context.data.safeAddress,
               firstName: context.data.circlesGardenProfile?.username,
-              avatarUrl: context.data.circlesGardenProfile?.avatarUrl
+              avatarUrl: context.data.circlesGardenProfile?.avatarUrl,
             };
           },
           onDone: "#success",
@@ -183,7 +212,10 @@ const processDefinition = (processId: string) =>
     },
   });
 
-export const importCirclesProfile: ProcessDefinition<void, ImportedCirclesProfileData> = {
+export const importCirclesProfile: ProcessDefinition<
+  void,
+  ImportedCirclesProfileData
+> = {
   name: "importCirclesProfile",
   stateMachine: <any>processDefinition,
 };
