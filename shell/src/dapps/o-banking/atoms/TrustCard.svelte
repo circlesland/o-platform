300177464afb2994656bbd7dd0dd48c33cbed42b
Thorsten Schau <letsgo@codeho.net>
2021-04-27 16:42:11 +0200

    fix #16 remove toggle trust from TrustCard

--- shell/src/dapps/o-banking/atoms/TrustCard.svelte
+++ shell/src/dapps/o-banking/atoms/TrustCard.svelte
@@ -91,46 +91,6 @@
       })
     );
   }
-
-  function execTrust(recipientAddress?: string) {
-    window.o.publishEvent(
-      new RunProcess<ShellProcessContext>(shellProcess, true, async (ctx) => {
-        ctx.childProcessDefinition = setTrust;
-        ctx.childContext = {
-          data: {
-            trustLimit: parseInt(limit) > 0 ? 0 : 100,
-            trustReceiver: recipientAddress,
-            safeAddress: tryGetCurrentSafe()?.safeAddress,
-            privateKey: localStorage.getItem("circlesKey"),
-          },
-          messages: {},
-          dirtyFlags: {},
-          environment: {},
-        };
-        return ctx;
-      })
-    );
-  }
-
-  function execUntrust(recipientAddress?: string) {
-    window.o.publishEvent(
-      new RunProcess<ShellProcessContext>(shellProcess, true, async (ctx) => {
-        ctx.childProcessDefinition = setTrust;
-        ctx.childContext = {
-          data: {
-            trustLimit: 0,
-            trustReceiver: recipientAddress,
-            safeAddress: tryGetCurrentSafe()?.safeAddress,
-            privateKey: localStorage.getItem("circlesKey"),
-          },
-          messages: {},
-          dirtyFlags: {},
-          environment: {},
-        };
-        return ctx;
-      })
-    );
-  }
 </script>
 
 <section class="flex items-center justify-center mb-2 text-circlesdarkblue">
@@ -178,15 +138,15 @@
         </div>
       </div>
 
-      <div class="flex flex-1 flex-col justify-items-end">
-        <div class="self-end flex flex-col space-y-2 text-2xl sm:text-3xl">
+      <div class="flex flex-1 flex-col justify-items-end self-start">
+        <div class="self-end flex flex-col space-y-2 text-2xl sm:text-3xl ">
           <button
             on:click={() => execTransfer(safeAddress)}
-            class="btn btn-square self-end btn-sm btn-primary"
+            class="btn btn-square self-end btn-md btn-primary"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
+              class="h-10 w-10"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -196,25 +156,6 @@
                 stroke-linejoin="round"
                 stroke-width="2"
                 d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-          </button>
-          <button
-            on:click={() => execTrust(safeAddress)}
-            class="btn btn-square self-end btn-sm btn-primary"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
               />
             </svg>
           </button>
