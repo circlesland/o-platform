bd6ea6281a72c78255933d82c437219f1c4fdfe6
Thorsten Schau <letsgo@codeho.net>
2021-04-28 15:13:05 +0200

    #28 made top of banking-header sticky

--- shell/src/dapps/o-banking/atoms/BankingHeader.svelte
+++ shell/src/dapps/o-banking/atoms/BankingHeader.svelte
@@ -17,41 +17,43 @@
 </script>
 
 <!-- BANKING HEADER START -->
+{#if lastLoadedDapp && lastLoadedPage}
+  <div
+    class="flex flex-row  justify-between navbar bg-gradient-to-r from-gradient1 to-gradient2 text-white sticky -top-0.5 z-10"
+  >
+    <div class=" pl-2 self-start">
+      <span class="text-lg font-circles "
+        >{#if lastLoadedDapp.title !== lastLoadedPage.title}
+          {lastLoadedDapp.title} /
+        {/if}{lastLoadedPage.title}</span
+      >
+    </div>
+    <div class="self-end">
+      <button
+        class=" text-white"
+        on:click|once={() => (window.location = "/#/dashboard")}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+          />
+        </svg>
+      </button>
+    </div>
+  </div>
+{/if}
 <div
   class="h-80 flex flex-col items-stretch navbar bg-gradient-to-r from-gradient1 to-gradient2 text-white"
 >
-  {#if lastLoadedDapp && lastLoadedPage}
-    <div class="flex flex-row  justify-between">
-      <div class=" pl-2 self-start">
-        <span class="text-lg font-circles "
-          >{#if lastLoadedDapp.title !== lastLoadedPage.title}
-            {lastLoadedDapp.title} /
-          {/if}{lastLoadedPage.title}</span
-        >
-      </div>
-      <div class="self-end">
-        <button
-          class=" text-white"
-          on:click|once={() => (window.location = "/#/dashboard")}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-            />
-          </svg>
-        </button>
-      </div>
-    </div>
-  {/if}
   <div class="self-center text-center mt-12 mb-8 block">
     <span class="block text-base-300">Your Balance</span>
     <span class="inline-block text-6xl font-circles ml-10 "
