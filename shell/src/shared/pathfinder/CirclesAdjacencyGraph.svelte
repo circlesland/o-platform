a27d03991818dc934c16a6bc6e23a9af29f50d4d
Thorsten Schau <letsgo@codeho.net>
2021-05-04 12:12:08 +0200

    #77 new logo and favicon

--- shell/src/shared/pathfinder/CirclesAdjacencyGraph.svelte
+++ shell/src/shared/pathfinder/CirclesAdjacencyGraph.svelte
@@ -1,63 +1,67 @@
 <script lang="ts">
-import {Network} from 'vis-network';
-import {DataSet} from 'vis-data';
-import { onMount } from 'svelte';
+  import { Network } from "vis-network";
+  import { DataSet } from "vis-data";
+  import { onMount } from "svelte";
 
-import { fillUsernames, userDB } from './userdb.js';
-import { toAddress, getAdjacencies } from './utility.js';
-import { createNodeContents } from './visUtils.js';
+  import { fillUsernames, userDB } from "./userdb.js";
+  import { toAddress, getAdjacencies } from "./utility.js";
+  import { createNodeContents } from "./visUtils.js";
 
-export let address:string = "";
+  export let address: string = "";
 
-// let usernameToExplore = '';
-let graph;
+  // let usernameToExplore = '';
+  let graph;
 
-let nodes = new DataSet();
-let edges = new DataSet();
-let knownEdges = {};
+  let nodes = new DataSet();
+  let edges = new DataSet();
+  let knownEdges = {};
 
-onMount(() => {
-    var network = new Network(graph, {nodes: nodes, edges: edges}, {});
-    network.on('click', (params) => {
-        if (!params.nodes || params.nodes.length == 0) return;
-        exploreNode(params.nodes[0])
+  onMount(() => {
+    var network = new Network(graph, { nodes: nodes, edges: edges }, {});
+    network.on("click", (params) => {
+      if (!params.nodes || params.nodes.length == 0) return;
+      exploreNode(params.nodes[0]);
     });
-    exploreNode(address)
-});
+    exploreNode(address);
+  });
 
-let small = function(node) { node['size'] = 10; return node; }
+  let small = function (node) {
+    node["size"] = 10;
+    return node;
+  };
 
-let exploreNode = async function(username) {
-    let adjacencies = await getAdjacencies(username.startsWith("0x") ? await toAddress(username) : username);
+  let exploreNode = async function (username) {
+    let adjacencies = await getAdjacencies(
+      username.startsWith("0x") ? await toAddress(username) : username
+    );
     let addressesToQuery = {};
     for (let edge of adjacencies) {
-        addressesToQuery[edge.user] = true;
-        addressesToQuery[edge.trusts] = true;
+      addressesToQuery[edge.user] = true;
+      addressesToQuery[edge.trusts] = true;
     }
     await fillUsernames(Object.keys(addressesToQuery));
     for (let edge of adjacencies) {
-        nodes.update(small(createNodeContents(edge.user)));
-        nodes.update(small(createNodeContents(edge.trusts)));
-        if (!knownEdges[edge.user + ',' + edge.trusts]) {
-            knownEdges[edge.user + ',' + edge.trusts] = true;
-            edges.update({
-                from: edge.user,
-                to: edge.trusts,
-                label: edge.percentage == 50 ? '' : edge.percentage + '%',
-                arrows: "to",
-                font: {align: "top"}
-            });
-        }
+      nodes.update(small(createNodeContents(edge.user)));
+      nodes.update(small(createNodeContents(edge.trusts)));
+      if (!knownEdges[edge.user + "," + edge.trusts]) {
+        knownEdges[edge.user + "," + edge.trusts] = true;
+        edges.update({
+          from: edge.user,
+          to: edge.trusts,
+          label: edge.percentage == 50 ? "" : edge.percentage + "%",
+          arrows: "to",
+          font: { align: "top" },
+        });
+      }
     }
-};
+  };
 
-let resetGraph = function() {
+  let resetGraph = function () {
     nodes.clear();
     edges.clear();
     knownEdges = {};
-}
-
+  };
 </script>
 
 <!-- TODO: Find a better fix for the graph-height. Problem is that this value must be known upon initialization. Also resize scenarios must be handled. -->
-<div bind:this={graph} class="w-full -mt-56 pt-40" style="height:100vh;"></div>
+<div bind:this={graph} class="w-full -mt-56 pt-40" style="height:100vh;" />
