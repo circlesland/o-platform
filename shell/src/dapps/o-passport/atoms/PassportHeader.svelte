313e36baad09d6e3c019fcc7336e265ff7727516
Thorsten Schau <letsgo@codeho.net>
2021-05-05 16:55:04 +0200

    #7 edit first/lastname

--- shell/src/dapps/o-passport/atoms/PassportHeader.svelte
+++ shell/src/dapps/o-passport/atoms/PassportHeader.svelte
@@ -174,6 +174,21 @@
       <h2>
         {profile ? profile.firstName : "Martin"}
         {profile && profile.lastName ? profile.lastName : ""}
+        <button
+          class="link link-primary text-primary text-2xs self-start -mt-2 -mr-3"
+          on:click={() => editProfileField({ firstName: true, lastName: true })}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-3 w-3 relative top-0 right-0"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+            />
+          </svg>
+        </button>
       </h2>
     </div>
     {#if $me && profile && $me.id == profile.id}
