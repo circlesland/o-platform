c3a703a4286f28453da2afb5adf468725495fc80
thorsten schau <letsgo@codeho.net>
2021-05-07 17:33:04 +0200

    XDAI Detail

--- shell/src/dapps/o-banking/pages/AssetDetail.svelte
+++ shell/src/dapps/o-banking/pages/AssetDetail.svelte
@@ -1,15 +1,58 @@
 <script lang="ts">
   import { Token } from "../data/circles/types";
   import TokenCard from "../atoms/TokenCard.svelte";
+  import XdaiAssetCard from "../atoms/XdaiAssetCard.svelte";
+
   import { mySafe } from "../stores/safe";
   import { BN } from "ethereumjs-util";
   import AssetsHeader from "../atoms/AssetsHeader.svelte";
+  import { RpcGateway } from "@o-platform/o-circles/dist/rpcGateway";
 
-  // export let params: {
-  //   symbol: string;
-  // };
+  export let params: {
+    symbol: string;
+  };
+  console.log(params);
+  let accountxDai = {
+    symbol: "xdai",
+    icon: "",
+    balance: "0",
+    variety: 1,
+  };
+  let safexDai = {
+    symbol: "xdai",
+    icon: "",
+    balance: "0",
+    variety: 1,
+  };
+
   let tokens: Token[];
+
   $: {
+    if (params.symbol == "xdai" && $mySafe && $mySafe.accountxDai) {
+      accountxDai = {
+        symbol: "xdai",
+        icon: "",
+        balance: parseFloat(
+          RpcGateway.get()
+            .utils.fromWei($mySafe.accountxDai, "ether")
+            .toString()
+        ).toFixed(2),
+        variety: 1,
+      };
+    }
+    if (params.symbol == "xdai" && $mySafe && $mySafe.xDaiBalance) {
+      safexDai = {
+        symbol: "xdai",
+        icon: "",
+        balance: parseFloat(
+          RpcGateway.get()
+            .utils.fromWei($mySafe.xDaiBalance, "ether")
+            .toString()
+        ).toFixed(2),
+        variety: 1,
+      };
+    }
+
     tokens = Object.values($mySafe.acceptedTokens.tokens).filter((o) =>
       new BN(o.balance).gt(new BN("0"))
     );
@@ -27,6 +70,15 @@
         </div>
       </div>
     </section>
+  {:else if params && params.symbol == "xdai"}
+    {#each [accountxDai, safexDai] as token}
+      <XdaiAssetCard
+        symbol={token.symbol}
+        balance={token.balance}
+        variety={token.variety}
+        colorClass="text-primary"
+      />
+    {/each}
   {:else}
     {#each tokens as token}
       {#if token && token.balance > 0}
