d06410118cf65280e6f5b421364ae40ad72571aa
thorsten schau <letsgo@codeho.net>
2021-05-09 14:00:49 +0200

    Closes #11 - beautiful techy details ;)

--- shell/tailwind.config.js
+++ shell/tailwind.config.js
@@ -39,6 +39,7 @@ module.exports = {
         circleshighlightblue: "#099BB0",
         circleslightblue: "#7c96a3",
         linkgrey: "#97acbc",
+        darkgrey: "#484b52",
         transactionpositive: "#32CD32",
         transactionnegative: "#DC143C",
         primary: "#0ad99c",
