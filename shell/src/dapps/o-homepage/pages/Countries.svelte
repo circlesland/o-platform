7437342a509ed5dd2031545158619181e28b2121
thorsten schau <letsgo@codeho.net>
2021-05-11 12:15:32 +0200

    homepage

--- shell/src/dapps/o-homepage/pages/Countries.svelte
+++ shell/src/dapps/o-homepage/pages/Countries.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  import Countries from "../components/Countries.svelte";
+</script>
+
 <div class="flex flex-col h-full">
   <main class="z-30 flex-1 overflow-y-visible">
     <Countries />
