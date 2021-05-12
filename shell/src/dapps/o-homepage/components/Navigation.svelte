b2db64777db17b1e2ad139ea2429592f35dc54fa
thorsten schau <letsgo@codeho.net>
2021-05-12 20:00:26 +0200

    #137 addes subpages to footer, added correct links, added LogIn button

--- shell/src/dapps/o-homepage/components/Navigation.svelte
+++ shell/src/dapps/o-homepage/components/Navigation.svelte
@@ -1,23 +1,114 @@
-<header class="px-4 py-2 mx-auto font-bold uppercase lg:py-4 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 font-circles">
-  <div class="relative flex items-center justify-between lg:justify-center lg:space-x-16">
+<script lang="ts">
+  import { RunProcess } from "@o-platform/o-process/dist/events/runProcess";
+  import {
+    shellProcess,
+    ShellProcessContext,
+  } from "../../../shared/processes/shellProcess";
+  import { Generate } from "@o-platform/o-utils/dist/generate";
+  import {
+    identify,
+    IdentifyContextData,
+  } from "../../o-passport/processes/identify/identify";
+
+  async function login() {
+    const requestEvent = new RunProcess<ShellProcessContext>(
+      shellProcess,
+      true,
+      async (ctx) => {
+        ctx.childProcessDefinition = identify;
+        ctx.childContext = {
+          data: <IdentifyContextData>{
+            redirectTo: "/dashboard",
+          },
+        };
+        return ctx;
+      }
+    );
+
+    requestEvent.id = Generate.randomHexString(8);
+    window.o.publishEvent(requestEvent);
+  }
+</script>
+
+<header class="flex px-4 py-2 w-full font-bold uppercase lg:py-4 font-circless">
+  <div
+    class="relative flex w-full items-center justify-between lg:justify-center lg:space-x-16"
+  >
     <ul class="flex items-center hidden space-x-8 lg:flex">
-      <li><a href="https://discord.gg/CS6xq7jECR" aria-label="Our product" title="Our product" class="font-bold tracking-wide transition-colors duration-200 text-secondary hover:text-primary">Chat</a></li>
-      <li><a href="https://aboutcircles.com" aria-label="Our product" title="Our product" class="font-bold tracking-wide transition-colors duration-200 text-secondary hover:text-primary">Forum</a></li>
+      <li>
+        <a
+          href="https://discord.gg/CS6xq7jECR"
+          target="_blank"
+          aria-label="Our product"
+          title="Our product"
+          class="font-bold tracking-wide transition-colors duration-200 text-secondary hover:text-primary"
+          >Chat</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://aboutcircles.com"
+          target="_blank"
+          aria-label="Our product"
+          title="Our product"
+          class="font-bold tracking-wide transition-colors duration-200 text-secondary hover:text-primary"
+          >Forum</a
+        >
+      </li>
     </ul>
-    <a href="/" aria-label="Company" title="Company" class="inline-flex items-center h-12">
-      <img src="/circles.png" alt="CirclesLAND" class="h-12">
+    <a
+      href="/"
+      aria-label="Company"
+      title="Company"
+      class="inline-flex items-center h-12"
+    >
+      <img src="/circles.png" alt="CirclesLAND" class="h-12" />
     </a>
     <ul class="flex items-center hidden space-x-8 lg:flex">
-      <li><a href="https://circlesland.ghost.io/" aria-label="About us" title="About us" class="font-bold tracking-wide transition-colors duration-200 text-secondary hover:text-primary">Blog</a></li>
-      <li><a href="https://circlesland.ghost.io/whitepaper/" aria-label="Sign in" title="Sign in" class="font-bold tracking-wide transition-colors duration-200 text-secondary hover:text-primary">Whitepaper</a></li>
+      <li>
+        <a
+          href="https://circlesland.ghost.io/"
+          target="_blank"
+          aria-label="About us"
+          title="About us"
+          class="font-bold tracking-wide transition-colors duration-200 text-secondary hover:text-primary"
+          >Blog</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://circlesland.ghost.io/whitepaper/"
+          target="_blank"
+          aria-label="Sign in"
+          title="Sign in"
+          class="font-bold tracking-wide transition-colors duration-200 text-secondary hover:text-primary"
+          >Whitepaper</a
+        >
+      </li>
     </ul>
+    <div class="absolute w-12 self-center justify-self-end right-8 lg:right-0">
+      <button class="btn-link" on:click={login}>Log in</button>
+    </div>
     <!-- Mobile menu -->
-    <div class="lg:hidden">
-      <button aria-label="Open Menu" title="Open Menu" class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50">
+    <div class="lg:hidden order-last">
+      <button
+        aria-label="Open Menu"
+        title="Open Menu"
+        class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+      >
         <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"></path>
-          <path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"></path>
-          <path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
+          <path
+            fill="currentColor"
+            d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+          />
+          <path
+            fill="currentColor"
+            d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+          />
+          <path
+            fill="currentColor"
+            d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+          />
         </svg>
       </button>
       <!-- Mobile menu dropdown 
