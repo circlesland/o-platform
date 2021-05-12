d83d285c0a0018b77da89eade4b470c7cb6d064d
thorsten schau <letsgo@codeho.net>
2021-05-12 10:07:55 +0200

    #127 added nav to all pages, fixed citizen view

--- shell/src/dapps/o-homepage/components/Dreams.svelte
+++ shell/src/dapps/o-homepage/components/Dreams.svelte
@@ -1,58 +1,100 @@
 <div class="bg-white">
-    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <div
+    class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
+  >
     <div class="mx-auto mb-10 lg:max-w-xl sm:text-center">
-      <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider uppercase rounded-full text-primary bg-teal-accent-400">
+      <p
+        class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider uppercase rounded-full text-primary bg-teal-accent-400"
+      >
         our citizens
       </p>
-      <h2 class="max-w-lg mb-6 text-3xl font-bold tracking-tight text-center font-circles text-secondary sm:text-4xl sm:leading-none">
+      <h2
+        class="max-w-lg mb-6 text-3xl font-bold tracking-tight text-center font-circles text-secondary sm:text-4xl sm:leading-none"
+      >
         Following their passions
       </h2>
       <p class="text-base text-gray-700 md:text-lg">
-        See the passion other Circlesland citizens are pursuing, when they receive their universal basic income in Circles
+        See the passion other Circlesland citizens are pursuing, when they
+        receive their universal basic income in Circles
       </p>
     </div>
-    <div class="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-3 lg:max-w-screen-lg">
+    <div
+      class="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-3 lg:max-w-screen-lg"
+    >
       <div>
-        <div class="relative pb-56 mb-4 lg:pb-64">
-          <img class="absolute object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=750" alt="Person" />
+        <div class="mb-4 text-center">
+          <div class="avatar">
+            <div class="rounded-full w-64 h-64">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=750"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
         <div class="flex flex-col sm:text-center">
           <p class="text-lg font-bold text-secondary">Oliver Aguilerra</p>
           <p class="mb-5 text-xs text-primary">Citizen 15 from Germany</p>
           <div class="flex items-center space-x-3 sm:justify-center">
-            ...is dreaming of "building the universal basic income economy of tomorrow, building the universal basic income"
+            ...is dreaming of "building the universal basic income economy of
+            tomorrow, building the universal basic income"
           </div>
         </div>
       </div>
       <div>
-        <div class="relative pb-56 mb-4 lg:pb-64">
-          <img class="absolute object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=750" alt="Person" />
+        <div class="mb-4 text-center">
+          <div class="avatar">
+            <div class="rounded-full w-64 h-64">
+              <img
+                src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=750"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
+
         <div class="flex flex-col sm:text-center">
           <p class="text-lg font-bold text-secondary">Marta Clermont</p>
           <p class="mb-5 text-xs text-primary">Citizen 16 from France</p>
           <div class="flex items-center space-x-3 sm:justify-center">
-            ...is dreaming of "building the universal basic income economy of tomorrow"
+            ...is dreaming of "building the universal basic income economy of
+            tomorrow"
           </div>
         </div>
       </div>
 
       <div>
-        <div class="relative pb-56 mb-4 lg:pb-64">
-          <img class="absolute object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=750" alt="Person" />
+        <div class="mb-4 text-center">
+          <div class="avatar">
+            <div class="rounded-full w-64 h-64">
+              <img
+                src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=750"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
+
         <div class="flex flex-col sm:text-center">
           <p class="text-lg font-bold text-secondary">Alice Melbourne</p>
           <p class="mb-5 text-xs text-primary">Citizen 17 from Australia</p>
           <div class="flex items-center space-x-3 sm:justify-center">
-            ...is dreaming of "building the universal basic income economy of tomorrow, building the universal basic income economy"
+            ...is dreaming of "building the universal basic income economy of
+            tomorrow, building the universal basic income economy"
           </div>
         </div>
       </div>
 
       <div>
-        <div class="relative pb-56 mb-4 lg:pb-64">
-          <img class="absolute object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=750" alt="Person" />
+        <div class="mb-4 text-center">
+          <div class="avatar">
+            <div class="rounded-full w-64 h-64">
+              <img
+                src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=750"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
         <div class="flex flex-col sm:text-center">
           <p class="text-lg font-bold text-secondary">Doris Auer</p>
@@ -64,38 +106,47 @@
       </div>
 
       <div>
-        <div class="relative pb-56 mb-4 lg:pb-64">
-          <img class="absolute object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/3931603/pexels-photo-3931603.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=750" alt="Person" />
+        <div class="mb-4 text-center">
+          <div class="avatar">
+            <div class="rounded-full w-64 h-64">
+              <img
+                src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=750"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
         <div class="flex flex-col sm:text-center">
           <p class="text-lg font-bold text-secondary">John Doe</p>
           <p class="mb-5 text-xs text-primary">Citizen 19 from Nigeria</p>
           <div class="flex items-center space-x-3 sm:justify-center">
-            ...is dreaming of "building the universal basic income economy of tomorrow, building the universal basic income economy of tomorrow"
+            ...is dreaming of "building the universal basic income economy of
+            tomorrow, building the universal basic income economy of tomorrow"
           </div>
         </div>
       </div>
 
       <div>
-        <div class="relative pb-56 mb-4 lg:pb-64">
-          <img class="absolute object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=750" alt="Person" />
+        <div class="mb-4 text-center">
+          <div class="avatar">
+            <div class="rounded-full w-64 h-64">
+              <img
+                src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=750"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
         <div class="flex flex-col sm:text-center">
           <p class="text-lg font-bold text-secondary">Li Pon</p>
           <p class="mb-5 text-xs text-primary">Citizen 20 from Japan</p>
           <div class="flex items-center space-x-3 sm:justify-center">
-            ...is dreaming of "building the universal basic income economy of tomorrow, building the universal basic income economy of tomorrow, building the universal basic income economy of tomorrow"
+            ...is dreaming of "building the universal basic income economy of
+            tomorrow, building the universal basic income economy of tomorrow,
+            building the universal basic income economy of tomorrow"
           </div>
         </div>
       </div>
-      
-
-      <a href="/#/citizens" aria-label="" class="inline-flex items-center w-full py-12 mx-auto mt-6 text-xl font-semibold text-center transition-colors duration-200 text-primary hover:text-secondary">
-        Show all citizens (please center)
-            <svg class="inline-block w-3 ml-2" fill="currentColor" viewBox="0 0 12 12">
-            <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z"></path>
-            </svg>
-        </a>
     </div>
   </div>
 </div>
