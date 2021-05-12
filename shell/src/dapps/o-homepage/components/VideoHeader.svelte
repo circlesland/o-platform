77321ae39d1e0193eeebd6485849c26ddd81b3d8
thorsten schau <letsgo@codeho.net>
2021-05-12 09:43:17 +0200

    #125 remove margin only shown in safari

--- shell/src/dapps/o-homepage/components/VideoHeader.svelte
+++ shell/src/dapps/o-homepage/components/VideoHeader.svelte
@@ -32,7 +32,7 @@
 >
   <div
     id="video-overlay"
-    class="z-50 absolute mx-2 text-center video-overlay h-full w-full"
+    class="z-50 absolute text-center video-overlay h-full w-full"
     on:click={() => playVideo()}
   >
     <h1
