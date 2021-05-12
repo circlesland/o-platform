51f21696a9ed01b22d7df79a57d41fa58cc8e206
thorsten schau <letsgo@codeho.net>
2021-05-12 10:07:55 +0200

    #127 added nav to all pages, fixed citizen view

--- shell/src/dapps/o-homepage/pages/Citizens.svelte
+++ shell/src/dapps/o-homepage/pages/Citizens.svelte
@@ -1,59 +1,13 @@
 <script lang="ts">
-  import { RunProcess } from "@o-platform/o-process/dist/events/runProcess";
   import Dreams from "../components/Dreams.svelte";
-  import {
-    shellProcess,
-    ShellProcessContext,
-  } from "../../../shared/processes/shellProcess";
-  import { Generate } from "@o-platform/o-utils/dist/generate";
-  import {
-    identify,
-    IdentifyContextData,
-  } from "../../o-passport/processes/identify/identify";
-  import { me } from "../../../shared/stores/me";
-
-  $: {
-    console.log($me); // TODO: This is just to init the store. There could be a better solution to do this :)
-  }
-
-  $: me;
-
-  async function login() {
-    const requestEvent = new RunProcess<ShellProcessContext>(
-      shellProcess,
-      true,
-      async (ctx) => {
-        ctx.childProcessDefinition = identify;
-        ctx.childContext = {
-          data: <IdentifyContextData>{
-            redirectTo: "/dashboard",
-          }
-        };
-        return ctx;
-      }
-    );
-
-    requestEvent.id = Generate.randomHexString(8);
-    window.o.publishEvent(requestEvent);
-  }
+  import Progress from "../components/Progress.svelte";
+  import Footer from "../components/Footer.svelte";
 </script>
 
-<div
-  class="flex flex-col h-full"
->
+<div class="flex flex-col h-full">
   <main class="z-30 flex-1 overflow-y-visible">
-  <Dreams />
+    <Dreams />
+    <Progress />
+    <Footer />
   </main>
-  <!-- <footer class="sticky bottom-0 z-50 w-full ">
-    <div class="flex justify-around ">
-      <button on:click={login} class="mb-4 btn btn-white">
-        <img
-          width="15px"
-          class="mr-3"
-          src="/images/common/circles.png"
-          alt="circles.land"
-        /> Login
-      </button>
-    </div>
-  </footer> -->
 </div>
--- shell/src/dapps/o-homepage/pages/Home.svelte
+++ shell/src/dapps/o-homepage/pages/Home.svelte
@@ -12,32 +12,6 @@
   import IntroAnimation from "../components/IntroAnimation.svelte";
 </script>
 
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/@vime/core@^5/themes/default.css"
-/>
-
-<!-- 
-<div class="flex flex-col h-screen text-base bg-white bg-gradient-to-r">
-  <main class="z-30 flex-1 overflow-y-visible">
-    <Navigation />
-    <div class="flex flex-col text-center justify-items-center pt-11">
-      <h1 class="m-auto font-bold font-circles text-gradient w-42">
-        You got us!
-      </h1>
-    </div>
-  </main>
-  <footer class="sticky bottom-0 z-50 w-full h-30 bg-secondary">
-    <div class="container flex justify-around w-full">
-      <div class="">left</div>
-      <button on:click={login} class="px-8 m-2 btn btn-primary">
-        Join us now
-      </button>
-      <div class="">right</div>
-    </div>
-  </footer>
-</div> -->
-
 <div class="flex flex-col h-screen">
   <Navigation />
   <main class="flex-1 overflow-y-auto">
