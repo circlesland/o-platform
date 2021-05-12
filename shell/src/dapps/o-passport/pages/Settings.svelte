23286f467a93fa473b06d5ea612e97ed1a38fc54
jaensen <daniel@djanz.de>
2021-05-12 23:41:49 +0200

    toggle newsletter subscription #8

--- shell/src/dapps/o-passport/pages/Settings.svelte
+++ shell/src/dapps/o-passport/pages/Settings.svelte
@@ -1,6 +1,50 @@
 <script lang="ts">
-  import {onMount} from "svelte";
   import PassportHeader from "../atoms/PassportHeader.svelte";
+  import {me} from "../../../shared/stores/me";
+  import {DelayedTrigger} from "@o-platform/o-utils/dist/delayedTrigger";
+  import {RunProcess} from "@o-platform/o-process/dist/events/runProcess";
+  import {shellProcess, ShellProcessContext} from "../../../shared/processes/shellProcess";
+  import {upsertIdentity} from "../processes/upsertIdentity";
+
+  function editProfile() {
+    const requestEvent = new RunProcess<ShellProcessContext>(
+            shellProcess,
+            true,
+            async (ctx) => {
+              ctx.childProcessDefinition = {
+                id: upsertIdentity.id,
+                name: upsertIdentity.name,
+                stateMachine: (processId?: string) =>
+                        (<any>upsertIdentity).stateMachine(processId, true),
+              };
+              ctx.childContext = {
+                data: {
+                  id: $me.id,
+                  circlesAddress: $me.circlesAddress,
+                  circlesSafeOwner: $me.circlesSafeOwner,
+                  avatarCid: $me.avatarCid,
+                  avatarUrl: $me.avatarUrl,
+                  avatarMimeType: $me.avatarMimeType,
+                  firstName: $me.firstName,
+                  lastName: $me.lastName,
+                  country: $me.country,
+                  dream: $me.dream,
+                  newsletter: receiveNewsletter
+                },
+                dirtyFlags: {},
+              };
+              return ctx;
+            }
+    );
+
+    window.o.publishEvent(requestEvent);
+  }
+
+  let receiveNewsletter:boolean = $me.newsletter;
+  const delayedTrigger = new DelayedTrigger(500, async () => {
+    // TODO: Make call to upsertProfile shorter
+    await editProfile();
+  });
 
 </script>
 
@@ -11,6 +55,17 @@
 
 <div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">email address</div>
 
-<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">newsletter</div>
+<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">
+  <span class="inline">
+    Newsletter
+  </span>
+  <input
+        name="checkbox"
+        id="newsletter"
+        type="checkbox"
+        bind:checked={receiveNewsletter}
+        on:change={() => delayedTrigger.trigger()}
+  />
+</div>
 
 <div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">tos</div>
