4d8e6314c4bd73619b9b84fe7b36811df6bfc8a3
thorsten schau <letsgo@codeho.net>
2021-05-13 12:11:41 +0200

    #130 adding hamburger menu and login button

---
+++ shell/src/dapps/o-homepage/atoms/Navigation.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Hamburger from "./Hamburger.svelte";
+  import Menu from "./Menu.svelte";
+
+  export let sidebar = false;
+</script>
+
+<header
+  class="flex justify-between bg-gray-200 p-2 items-center text-gray-600 border-b-2"
+>
+  <nav class="mt-2 z-50 lg:hidden">
+    <Hamburger bind:open={sidebar} />
+  </nav>
+
+  <Menu />
+</header>
