4ac94e11308820d6a462acf05b50cefecda64cf3
thorsten schau <letsgo@codeho.net>
2021-05-13 13:23:22 +0200

    #154 - fixed. unfortunately to accomodate iOS Safari, the Footer has to be above the nav when scrolled all the way to the bottom - this though also fixes the double scrollbars

--- shell/src/dapps/o-homepage/components/Footer.svelte
+++ shell/src/dapps/o-homepage/components/Footer.svelte
@@ -1,4 +1,4 @@
-<div class="text-white bg-darkblue">
+<footer class="text-white bg-darkblue">
   <div
     class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8"
   >
@@ -140,4 +140,4 @@
       </div>
     </div>
   </div>
-</div>
+</footer>
