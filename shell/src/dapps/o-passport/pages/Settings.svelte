c5efd42989fc22b2cb341c9d323ae2828317f522
thorsten schau <letsgo@codeho.net>
2021-05-14 13:37:01 +0200

    #8 styled settings page

--- shell/src/dapps/o-passport/pages/Settings.svelte
+++ shell/src/dapps/o-passport/pages/Settings.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import PassportHeader from "../atoms/PassportHeader.svelte";
-  import {me} from "../../../shared/stores/me";
-  import {DelayedTrigger} from "@o-platform/o-utils/dist/delayedTrigger";
-  import {onMount} from "svelte";
-  import {UpsertProfileDocument, WhoamiDocument} from "../data/api/types";
+  import { me } from "../../../shared/stores/me";
+  import { DelayedTrigger } from "@o-platform/o-utils/dist/delayedTrigger";
+  import { onMount } from "svelte";
+  import { UpsertProfileDocument, WhoamiDocument } from "../data/api/types";
 
   async function editProfile() {
     /*
@@ -54,53 +54,76 @@
         lastName: $me.lastName,
         country: $me.country,
         dream: $me.dream,
-        newsletter: receiveNewsletter
-      }
+        newsletter: receiveNewsletter,
+      },
     });
     if (result.errors) {
       return;
     }
   }
 
-  let email:string = "unknown";
+  let email: string = "unknown";
 
   onMount(async () => {
     const apiClient = await window.o.apiClient.client.subscribeToResult();
     const result = await apiClient.query({
-      query: WhoamiDocument
+      query: WhoamiDocument,
     });
     if (result.errors) {
       return;
     }
     email = result.data.whoami;
-  })
+  });
 
-  let receiveNewsletter:boolean = $me.newsletter;
+  let receiveNewsletter: boolean = $me.newsletter;
   const delayedTrigger = new DelayedTrigger(500, async () => {
     // TODO: Make call to upsertProfile shorter
     await editProfile();
   });
-
 </script>
 
 <PassportHeader />
-<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">
-  SETTINGS<br />
+
+<div class="mx-4 -mt-6">
+  <section class="flex items-center justify-center mb-2 text-circlesdarkblue ">
+    <div class="flex flex-col bg-white shadow p-4 w-full space-y-2 rounded-sm">
+      <div class="text-primary text-xs font-circles font-bold text-left">
+        EMAIL
+      </div>
+      <div class="flex items-center bg-white w-full space-x-2 sm:space-x-6">
+        <div class="mr-2 text-center">
+          {email}
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="flex items-center justify-center mb-2 text-circlesdarkblue ">
+    <div class="flex flex-col bg-white shadow p-4 w-full space-y-2 rounded-sm">
+      <div class="text-primary text-xs font-circles font-bold text-left">
+        NEWSLETTER
+      </div>
+      <div class="flex items-center bg-white w-full space-x-2 sm:space-x-6">
+        <div class="form-control">
+          <label
+            class="label flex items-center flex-row cursor-pointer space-x-10"
+            for="newsletter"
+          >
+            <span class="inline-block self-center">Receive Newsletter</span>
+            <div class="inline-block self-center relative top-1">
+              <input
+                name="checkbox"
+                id="newsletter"
+                type="checkbox"
+                class="inline-block toggle toggle-primary"
+                bind:checked={receiveNewsletter}
+                on:change={() => delayedTrigger.trigger()}
+              />
+
+              <span class="toggle-mark" />
+            </div>
+          </label>
+        </div>
+      </div>
+    </div>
+  </section>
 </div>
-
-<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">email address<br/>{email}</div>
-
-<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">
-  <span class="inline">
-    Newsletter
-  </span>
-  <input
-        name="checkbox"
-        id="newsletter"
-        type="checkbox"
-        bind:checked={receiveNewsletter}
-        on:change={() => delayedTrigger.trigger()}
-  />
-</div>
-
-<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">tos</div>
