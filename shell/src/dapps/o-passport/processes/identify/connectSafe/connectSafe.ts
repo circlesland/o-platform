29775194553113641141817977e8d1dfd94d3b8c
jaensen <daniel@djanz.de>
2021-05-27 01:04:15 +0200

    closes #275

--- shell/src/dapps/o-passport/processes/identify/connectSafe/connectSafe.ts
+++ shell/src/dapps/o-passport/processes/identify/connectSafe/connectSafe.ts
@@ -1,6 +1,6 @@
 import { ProcessDefinition } from "@o-platform/o-process/dist/interfaces/processManifest";
 import { ProcessContext } from "@o-platform/o-process/dist/interfaces/processContext";
-import TextEditor from "@o-platform/o-editors/src/TextEditor.svelte";
+import DropdownSelectEditor from "@o-platform/o-editors/src/DropdownSelectEditor.svelte";
 import TextareaEditor from "@o-platform/o-editors/src/TextareaEditor.svelte";
 import { prompt } from "@o-platform/o-process/dist/states/prompt";
 import { fatalError } from "@o-platform/o-process/dist/states/fatalError";
@@ -8,9 +8,12 @@ import { createMachine } from "xstate";
 import { GnosisSafeProxy } from "@o-platform/o-circles/dist/safe/gnosisSafeProxy";
 import { RpcGateway } from "@o-platform/o-circles/dist/rpcGateway";
 import * as bip39 from "bip39";
+import {Account} from "web3-core";
+import gql from "graphql-tag";
 
 export type ConnectSafeContextData = {
   safeAddress?: string;
+  foundSafeAddresses?: string[];
   safeOwners?: string[];
   accountAddress?: string;
   seedPhrase?: string;
@@ -30,76 +33,12 @@ const strings = {
 const processDefinition = (processId: string) =>
   createMachine<ConnectSafeContext, any>({
     id: `${processId}`,
-    initial: "checkSkipSafeAddress",
+    initial: "seedPhrase",
     states: {
       // Include a default 'error' state that propagates the error by re-throwing it in an action.
       // TODO: Check if this works as intended
       ...fatalError<ConnectSafeContext, any>("error"),
 
-      checkSkipSafeAddress: {
-        always: [
-          {
-            cond: (context) => {
-              return !!context.data.safeAddress;
-            },
-            target: "#checkSafeAddress",
-          },
-          {
-            target: "#safeAddress",
-          },
-        ],
-      },
-      safeAddress: prompt<ConnectSafeContext, any>({
-        fieldName: "safeAddress",
-        component: TextEditor,
-        params: {
-          label: strings.labelSafeAddress,
-          placeholder: strings.placeholderSafeAddress,
-          submitButtonText: "Connect",
-        },
-        navigation: {
-          next: "#checkSafeAddress",
-        },
-      }),
-      checkSafeAddress: {
-        id: "checkSafeAddress",
-        invoke: {
-          src: async (context) => {
-            context.messages["safeAddress"] = ``;
-            context.data.safeAddress = context.data.safeAddress?.trim();
-            try {
-              console.log(
-                `Checking if safe ${context.data.safeAddress} exists ..`
-              );
-              await RpcGateway.trigger(async (web3) => {
-                const safeProxy = new GnosisSafeProxy(
-                  web3,
-                  context.data.safeAddress
-                );
-                context.data.safeOwners = await safeProxy.getOwners();
-                console.log(
-                  `Checking if safe ${context.data.safeAddress} exists .. Safe exists.`
-                );
-              }, 2500);
-              return true;
-            } catch (e) {
-              if (e.message == "slow_provider") {
-                throw e;
-              }
-              context.messages[
-                "safeAddress"
-              ] = `Couldn't determine the owner of safe ${context.data.safeAddress}. Is the address right?`;
-              console.log(
-                `Checking if safe ${context.data.safeAddress} exists .. Safe doesn't exist.`,
-                e
-              );
-              throw e;
-            }
-          },
-          onDone: "#seedPhrase",
-          onError: "#safeAddress",
-        },
-      },
       seedPhrase: prompt<ConnectSafeContext, any>({
         fieldName: "seedPhrase",
         component: TextareaEditor,
@@ -110,47 +49,59 @@ const processDefinition = (processId: string) =>
           submitButtonText: "Store on this device",
         },
         navigation: {
-          next: "#prepareOutput",
+          next: "#checkSeedphrase",
         },
       }),
-      prepareOutput: {
-        id: "prepareOutput",
+      checkSeedphrase: {
+        id: "checkSeedphrase",
         invoke: {
           src: async (context) => {
             context.messages["seedPhrase"] = "";
 
             let keyFromMnemonic: string;
-            let account: any;
+            let account: Account;
 
             try {
-              keyFromMnemonic =
-                "0x" + bip39.mnemonicToEntropy(context.data.seedPhrase);
+              keyFromMnemonic = "0x" + bip39.mnemonicToEntropy(context.data.seedPhrase);
             } catch (e) {
-              context.messages[
-                "seedPhrase"
-              ] = `The seedphrase cannot be converted to a private key. Please double check it.`;
+              context.messages["seedPhrase"] = `The seedphrase cannot be converted to a private key. Please double check it.`;
               throw e;
             }
 
             try {
-              account =
-                RpcGateway.get().eth.accounts.privateKeyToAccount(
-                  keyFromMnemonic
-                );
+              account = RpcGateway.get().eth.accounts.privateKeyToAccount(keyFromMnemonic);
             } catch (e) {
-              context.messages[
-                "seedPhrase"
-              ] = `The key that was generated from the seedphrase cannot be converted to an ethereum account.`;
+              context.messages["seedPhrase"] = `The key that was generated from the seedphrase cannot be converted to an ethereum account.`;
               throw e;
             }
 
-            if (!context.data.safeOwners.find((o) => o === account.address)) {
-              context.messages[
-                "seedPhrase"
-              ] = `The given key doesn't belong to a owner of safe ${context.data.safeAddress}`;
-              throw new Error(
-                `The given key doesn't belong to a owner of safe ${context.data.safeAddress}`
-              );
+            if (!context.data.safeAddress) {
+              // If the flow was initialized without safe-address ..
+              const graph = await window.o.theGraphClient.client.subscribeToResult();
+              const foundSafes = await graph.query({
+                query: gql`query user($id:String!) {
+                  user(id: $id) {
+                    id
+                    safeAddresses
+                  }
+                }`,
+                variables: {
+                  id: account.address.toLowerCase()
+                }
+              });
+
+              if (foundSafes.errors && foundSafes.errors.length) {
+                const msg = `An error occurred while we tried to find your safe: ${JSON.stringify(foundSafes.errors)}`;
+                context.messages["seedPhrase"] = msg;
+                throw new Error(msg)
+              }
+
+              context.data.foundSafeAddresses = foundSafes.data.user?.safeAddresses ?? [];
+              if (!context.data.foundSafeAddresses.length) {
+                const msg = `We couldn't find a safe for your account ${account.address}`;
+                context.messages["seedPhrase"] = msg;
+                throw new Error(msg)
+              }
             }
 
             localStorage.setItem("circlesAccount", account.address);
@@ -158,8 +109,77 @@ const processDefinition = (processId: string) =>
             context.data.accountAddress = account.address;
             context.data.privateKey = keyFromMnemonic;
           },
+          onDone: [{
+            cond: (context) => (context.messages["seedPhrase"]?.trim() ?? "") !== "",
+            target: "#seedPhrase"
+          }, {
+            cond: (context) => context.data.foundSafeAddresses?.length > 1,
+            target: "#safeAddress"
+          }, {
+            cond: (context) => context.data.foundSafeAddresses?.length == 1,
+            actions: (context) => context.data.safeAddress = context.data.foundSafeAddresses[0],
+            target: "#checkSafeAddress"
+          }, {
+            target: "#checkSafeAddress"
+          }],
+          onError: "#seedPhrase"
+        }
+      },
+      safeAddress: prompt({
+        fieldName: "safeAddress",
+        component: DropdownSelectEditor,
+        params: (context) => {
+          return {
+            label: "We found multiple safes for your account. Please select the one you want to connect.",
+            placeholder: "Click to select a safe",
+            submitButtonText: "Connect",
+            optionIdentifier: "value",
+            getOptionLabel: (option) => option,
+            getSelectionLabel: (option) => option,
+            choices: context.data.foundSafeAddresses
+          }
+        },
+        navigation: {
+          next: "#checkSafeAddress",
+        }
+      }),
+      checkSafeAddress: {
+        id: "checkSafeAddress",
+        invoke: {
+          src: async (context) => {
+            context.messages["safeAddress"] = ``;
+            context.data.safeAddress = context.data.safeAddress?.trim();
+            try {
+              console.log(
+                  `Checking if safe ${context.data.safeAddress} exists ..`
+              );
+              await RpcGateway.trigger(async (web3) => {
+                const safeProxy = new GnosisSafeProxy(
+                    web3,
+                    context.data.safeAddress
+                );
+                context.data.safeOwners = await safeProxy.getOwners();
+                console.log(
+                    `Checking if safe ${context.data.safeAddress} exists .. Safe exists.`
+                );
+              }, 2500);
+              return true;
+            } catch (e) {
+              if (e.message == "slow_provider") {
+                throw e;
+              }
+              context.messages[
+                  "safeAddress"
+                  ] = `Couldn't determine the owner of safe ${context.data.safeAddress}. Is the address right?`;
+              console.log(
+                  `Checking if safe ${context.data.safeAddress} exists .. Safe doesn't exist.`,
+                  e
+              );
+              throw e;
+            }
+          },
           onDone: "#success",
-          onError: "#seedPhrase",
+          onError: "#safeAddress",
         },
       },
       success: {
