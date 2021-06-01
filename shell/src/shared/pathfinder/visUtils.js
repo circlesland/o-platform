6eb6fc24d7f8940fcf29506b876ac83575f14dc2
Thorsten Schau <letsgo@codeho.net>
2021-06-01 17:15:32 +0200

    #373 Transfer Graph changes.

--- shell/src/shared/pathfinder/visUtils.js
+++ shell/src/shared/pathfinder/visUtils.js
@@ -1,20 +1,26 @@
-import { userDB } from './userdb.js';
-import {AvataarGenerator} from "src/shared/avataarGenerator";
+import { userDB } from "./userdb.js";
+import { AvataarGenerator } from "src/shared/avataarGenerator";
 
-export let labelFor = function(id) {
-    let user = userDB[id.toLowerCase()];
-    if (user && user.username)
-        return user.username;
-    else
-        return id.substr(0, 8);
+export let labelFor = function (id) {
+  let user = userDB[id.toLowerCase()];
+  if (user && user.username) return user.username;
+  else return id.substr(0, 8);
 };
 
-export let createNodeContents = function(id) {
-    let user = userDB[id.toLowerCase()];
-    let node = {id: id, label: labelFor(id)};
-    node['shape'] = 'circularImage';
-    node['image'] = user && user.avatarUrl
-        ? user.avatarUrl
-        : AvataarGenerator.generate(id);
-    return node;
-}
+export let createNodeContents = function (id) {
+  let user = userDB[id.toLowerCase()];
+  let node = { id: id, label: labelFor(id) };
+  node["shape"] = "circularImage";
+  node["image"] =
+    user && user.avatarUrl ? user.avatarUrl : AvataarGenerator.generate(id);
+
+  node["color"] = {
+    border: "#0ad99c",
+    background: "#0ad99c",
+  };
+  node["font"] = {
+    color: "#001c6e",
+  };
+
+  return node;
+};
