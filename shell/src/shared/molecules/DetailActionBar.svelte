c62b562d7db66a3a72d17e74f83e907c0ea25602
jaensen <daniel@djanz.de>
2021-06-28 19:52:29 +0200

    supply lastLoadedDapp to action handler

--- shell/src/shared/molecules/DetailActionBar.svelte
+++ shell/src/shared/molecules/DetailActionBar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Icons from "./Icons.svelte";
+  import {getLastLoadedDapp} from "src/loader";
 
   export let actions: any;
 
@@ -9,7 +10,7 @@
 <div class="flex flex-row justify-between p-2 text-secondary">
   {#each actions as action}
     <div
-      on:click={() => window.o.publishEvent(action.event(undefined))}
+      on:click={() => window.o.publishEvent(action.event(getLastLoadedDapp()))}
       class="flex flex-col items-center content-center self-start space-y-2 cursor-pointer"
     >
       <div>
