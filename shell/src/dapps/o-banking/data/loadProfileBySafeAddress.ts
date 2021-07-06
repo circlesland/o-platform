223aa0a6512c77d33ca8bb3d9d2e7c2dc1534640
jaensen <daniel@djanz.de>
2021-07-06 06:02:42 +0200

    hotfix: don't crash when api.circles.garden is down.

--- shell/src/dapps/o-banking/data/loadProfileBySafeAddress.ts
+++ shell/src/dapps/o-banking/data/loadProfileBySafeAddress.ts
@@ -48,22 +48,36 @@ export async function loadProfileBySafeAddress(safeAddress: string) : Promise<Ap
 
     // 2. Try to find a profile via circles garden
     const requestUrl = `https://api.circles.garden/api/users/?address[]=${safeAddress}`;
-    const gardenResult = await fetch(requestUrl);
-    if (gardenResult && gardenResult.status == 200) {
-        const resultJson = await gardenResult.json();
-        const profile =
-            resultJson.data && resultJson.data.length
-                ? resultJson.data[0]
-                : undefined;
-        return {
-            circlesAddress: RpcGateway.get().utils.toChecksumAddress(safeAddress),
-            firstName: profile ? profile.username : "",
-            avatarUrl: profile ? profile.avatarUrl : undefined,
-        };
+    try {
+        const gardenResult = await fetch(requestUrl);
+        if (gardenResult && gardenResult.status == 200) {
+            const resultJson = await gardenResult.json();
+            const profile =
+                resultJson.data && resultJson.data.length
+                    ? resultJson.data[0]
+                    : undefined;
+
+            if (!profile) {
+                return {
+                    id: 0,
+                    circlesAddress: RpcGateway.get().utils.toChecksumAddress(safeAddress),
+                    firstName: ""
+                }
+            }
+            return {
+                id: profile.id,
+                circlesAddress: RpcGateway.get().utils.toChecksumAddress(safeAddress),
+                firstName: profile ? profile.username : "",
+                avatarUrl: profile ? profile.avatarUrl : undefined
+            };
+        }
+    } catch (e) {
+        console.error(`Couldn't load the profile metadata from api.circles.garden. RequestUrl: ${requestUrl}`);
     }
 
     // 3. No profile found
     return {
+        id: 0,
         circlesAddress: RpcGateway.get().utils.toChecksumAddress(safeAddress),
         firstName: "",
     };
