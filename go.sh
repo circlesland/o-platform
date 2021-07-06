272a159780d3724ed7a46ba17090d1189b2b3d8b
Thorsten Schau <letsgo@codeho.net>
2021-07-06 10:17:39 +0200

    design

---
+++ go.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+export DEPLOY_ENVIRONMENT=$1
+
+echo "Installing build dependencies .."
+npm i
+npx --no-install lerna bootstrap || exit
+
+echo "Building 'o-utils' .."
+cd packages/o-utils || exit
+npx --no-install tsc || exit
+cd .. || exit
+
+echo "Building 'o-interfaces' .."
+cd o-interfaces || exit
+npx --no-install tsc || exit
+cd .. || exit
+
+echo "Building 'o-events' .."
+rm -r -f o-events/dist
+cd o-events || exit
+npx --no-install tsc || exit
+cd .. || exit
+
+echo "Building 'o-process' .."
+cd o-process || exit
+npx --no-install tsc || exit
+cd .. || exit
+
+echo "Building 'o-circles' .."
+cd o-circles || exit
+npx --no-install tsc || exit
+cd ../.. || exit
+
+echo "Generating graphql types for shared/api/data"
+echo "* api"
+cd shell/src/shared/api/data
+npx graphql-codegen
+echo "* api"
+npx graphql-codegen
+
+cd ../../../../..
+echo "Generating graphql types for dapps/o-passport"
+echo "* api"
+cd shell/src/dapps/o-passport/data/api
+npx graphql-codegen
+echo "* auth"
+cd ../auth
+npx graphql-codegen
+
+cd ../../../../../..
+echo "Generating graphql types for dapps/o-banking"
+echo "* api"
+cd shell/src/dapps/o-banking/data/api
+npx graphql-codegen
+
+cd ../../../../../..
+echo "Generating graphql types for dapps/o-marketplace"
+echo "* api"
+cd shell/src/dapps/o-marketplace/data/api
+npx graphql-codegen
+
+cd ../../../../../shell
+echo "Starting Webpack"
+npm run dev
--- shell/src/shared/molecules/NextNav/Components/Backlink.svelte
+++ shell/src/shared/molecules/NextNav/Components/Backlink.svelte
@@ -1,6 +1,10 @@
+<script>
+  // TODO: Edge Case back button on direct per url call of the page.. !!!
+</script>
+
 <div
-  class="w-24 h-12 py-3 -ml-4 bg-white rounded-full cursor-pointer px-11 xs:-ml-10 sm:-ml-12"
+  class="w-32 h-12 px-5 py-3 -mr-4 bg-white rounded-full cursor-pointer xs:-mr-10 sm:-mr-12 rightpill"
   on:click={() => window.history.back()}
 >
-  <div class="pl-2 xs:pl-0">Back</div>
+  <div class="pt-1 pr-2 xs:pr-0">Back</div>
 </div>
