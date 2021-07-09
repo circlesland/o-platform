8aacafd689923e3164f79c8c1ebb251a719e6980
Thorsten Schau <letsgo@codeho.net>
2021-07-09 10:29:05 +0200

    Add 16:9 image crop for Marketplace via param

--- shell/src/shared/api/promptFile.ts
+++ shell/src/shared/api/promptFile.ts
@@ -1,5 +1,5 @@
 import { ProcessContext } from "@o-platform/o-process/dist/interfaces/processContext";
-import { StateSchema} from "xstate";
+import { StateSchema } from "xstate";
 import { ipc } from "@o-platform/o-process/dist/triggers/ipc";
 import { PlatformEvent } from "@o-platform/o-events/dist/platformEvent";
 import { uploadFile } from "./uploadFile";
@@ -12,54 +12,57 @@ import PictureEditor from "@o-platform/o-editors/src/PictureEditor.svelte";
 import PicturePreview from "@o-platform/o-editors/src/PicturePreview.svelte";
 import HtmlViewer from "@o-platform/o-editors/src/HtmlViewer.svelte";
 import { Generate } from "@o-platform/o-utils/dist/generate";
-import {StateNodeConfig} from "xstate/lib/types";
+import { StateNodeConfig } from "xstate/lib/types";
 
 const strings = {
   labelFile: `Please select a file to upload`,
 };
 
 type UploadPictureSpec<TContext extends ProcessContext<any>> = {
-  id?:string;
+  id?: string;
   field: PromptField<TContext>;
-  onlyWhenDirty?: boolean,
-  uploaded?:(context:TContext, event:{
-    data: {
-      url: string
-      mimeType: string
-      fileName: string
+  onlyWhenDirty?: boolean;
+  uploaded?: (
+    context: TContext,
+    event: {
+      data: {
+        url: string;
+        mimeType: string;
+        fileName: string;
+      };
     }
-  }) => void,
+  ) => void;
   params?: {
-    label: string,
-    submitButtonText: string
-  },
+    label: string;
+    submitButtonText: string;
+    cropShape?: string;
+  };
   navigation: {
     // If you want to allow the user to go one step back then specify here where he came from
     previous?: string;
     canGoBack?: (
-        context: ProcessContext<TContext>,
-        event: { type: string; [x: string]: any }
+      context: ProcessContext<TContext>,
+      event: { type: string; [x: string]: any }
     ) => boolean;
     next: string;
     skip?: string;
     canSkip?: (
-        context: ProcessContext<TContext>,
-        event: { type: string; [x: string]: any }
+      context: ProcessContext<TContext>,
+      event: { type: string; [x: string]: any }
     ) => boolean;
   };
-}
+};
 
 export function promptFile<
-    TContext extends ProcessContext<any>,
-    TEvent extends PlatformEvent
-    >(spec: UploadPictureSpec<TContext>) {
-
+  TContext extends ProcessContext<any>,
+  TEvent extends PlatformEvent
+>(spec: UploadPictureSpec<TContext>) {
   // This variable will be used to back the 'file' value which
   // usually would be on context.data.file but the
   // PromptField's behaviour is overridden so that it uses
   // this field instead (see 'editFile'.'field' for details).
   let file: {
-    mimeType:string;
+    mimeType: string;
     fileName: string;
     bytes: Buffer;
   };
@@ -67,7 +70,7 @@ export function promptFile<
   const field = normalizePromptField(spec.field);
   spec.id = spec.id ? spec.id : field.name;
   const generatedId = Generate.randomHexString(4);
-  const id = (x:string) => `${spec.id}/${generatedId}/${x}`;
+  const id = (x: string) => `${spec.id}/${generatedId}/${x}`;
   const editDataFieldConfig: StateNodeConfig<TContext, StateSchema, TEvent> = {
     id: spec.id,
     initial: "checkPreviewFile",
@@ -75,14 +78,17 @@ export function promptFile<
       checkPreviewFile: {
         id: id("checkPreviewFile"),
         entry: () => console.log(`checkPreviewFile entry`),
-        always: [{
-          cond: (context) => {
-            return !!context.data[field.name];
+        always: [
+          {
+            cond: (context) => {
+              return !!context.data[field.name];
+            },
+            target: `#${id("previewFile")}`,
           },
-          target: `#${id("previewFile")}`
-        }, {
-          target: `#${id("checkEditFile")}`
-        }],
+          {
+            target: `#${id("checkEditFile")}`,
+          },
+        ],
       },
       previewFile: prompt<TContext, any>({
         id: id("previewFile"),
@@ -99,25 +105,28 @@ export function promptFile<
           canGoBack: () => !!spec.navigation.previous,
           previous: spec.navigation.previous,
           canSkip: spec.navigation.canSkip,
-          skip: spec.navigation.next
+          skip: spec.navigation.next,
         },
       }),
       checkEditFile: {
         id: id("checkEditFile"),
         entry: () => console.log(`checkEditFile entry`),
-        always: [{
-          cond: (context) => {
-            return !field.get(context);
+        always: [
+          {
+            cond: (context) => {
+              return !field.get(context);
+            },
+            actions: (context) => {
+              context.dirtyFlags["file"] = context.dirtyFlags[field.name];
+              delete context.dirtyFlags[field.name];
+              context.data.file = undefined;
+            },
+            target: `#${id("editFile")}`,
           },
-          actions: (context) => {
-            context.dirtyFlags["file"] = context.dirtyFlags[field.name];
-            delete context.dirtyFlags[field.name];
-            context.data.file = undefined;
+          {
+            target: spec.navigation.next,
           },
-          target: `#${id("editFile")}`,
-        }, {
-          target: spec.navigation.next,
-        }],
+        ],
       },
       editFile: prompt<TContext, any>({
         id: id("editFile"),
@@ -131,19 +140,20 @@ export function promptFile<
           set: (o) => {
             console.log("Setting 'file' to:", o);
             file = o;
-          }
+          },
         },
         onlyWhenDirty: spec.onlyWhenDirty,
         component: PictureEditor,
         params: {
           label: spec.params?.label ?? strings.labelFile,
           submitButtonText: "Save Image",
+          cropShape: spec.params?.cropShape ?? null,
         },
         navigation: {
           next: `#${id("uploadOrSkip")}`,
           previous: spec.navigation.previous,
           canSkip: spec.navigation.canSkip,
-          skip: spec.navigation.skip ?? spec.navigation.next
+          skip: spec.navigation.skip ?? spec.navigation.next,
         },
       }),
       uploadOrSkip: {
@@ -152,24 +162,21 @@ export function promptFile<
         always: [
           {
             cond: (context) => {
-              return (
-                  context.dirtyFlags["file"] &&
-                  !!file &&
-                  !!file.bytes
-              );
+              return context.dirtyFlags["file"] && !!file && !!file.bytes;
             },
             target: `#${id("uploadFile")}`,
           },
           {
-            cond: (context) => !context.dirtyFlags[field.name] && spec.onlyWhenDirty,
+            cond: (context) =>
+              !context.dirtyFlags[field.name] && spec.onlyWhenDirty,
             target: spec.navigation.skip ?? spec.navigation.next,
           },
           {
             actions: (context: TContext) => {
               context.messages[field.name] = `Please specify a valid file.`;
             },
-            target: `#${id("checkPreviewFile")}`
-          }
+            target: `#${id("checkPreviewFile")}`,
+          },
         ],
       },
       uploadFile: {
@@ -181,7 +188,7 @@ export function promptFile<
           console.log(`uploadFile entry`);
           window.o.publishEvent(<PlatformEvent>{
             type: "shell.progress",
-            message: `Uploading your file ..`
+            message: `Uploading your file ..`,
           });
         },
         invoke: {
@@ -227,13 +234,13 @@ export function promptFile<
         isSensitive: true,
         params: {
           submitButtonText: "Try again",
-          html: (context) => context.data.errorUploadingFile
+          html: (context) => context.data.errorUploadingFile,
         },
         navigation: {
-          next: `#${id("checkPreviewFile")}`
+          next: `#${id("checkPreviewFile")}`,
         },
-      })
-    }
+      }),
+    },
   };
   return editDataFieldConfig;
 }
