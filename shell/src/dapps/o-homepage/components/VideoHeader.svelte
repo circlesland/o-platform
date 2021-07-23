4aa751b1b33dafe3fc5aadba40a1ece2376a4cd8
Thorsten Schau <letsgo@codeho.net>
2021-07-23 09:59:39 +0200

    Fix fullscreen stuff for Safari also

--- shell/src/dapps/o-homepage/components/VideoHeader.svelte
+++ shell/src/dapps/o-homepage/components/VideoHeader.svelte
@@ -14,60 +14,50 @@
     player = document.querySelector("vm-player");
 
     player.addEventListener("vmPlay", (event) => {
-      player.enterFullscreen();
-      console.log("playing");
-
       document.getElementById("video-overlay").style.display = "none";
     });
 
     player.addEventListener("vmPausedChange", (event) => {
-      console.log("EVENT ", event);
       if (event.detail == true) {
-        console.log("EXITING FULLSCREEN");
         player.exitFullscreen();
       }
       document.getElementById("video-overlay").style.display = "block";
     });
 
-    // }
-
     document.addEventListener("fullscreenchange", (event) => {
-      console.log("fullscreen change", event);
       // document.fullscreenElement will point to the element that
       // is in fullscreen mode if there is one. If there isn't one,
       // the value of the property is null.
       if (document.fullscreenElement) {
         isFullscreen = true;
         document.getElementById("video-overlay").style.display = "none";
-        console.log(
-          `Element: ${document.fullscreenElement} entered full-screen mode.`
-        );
       } else {
-        console.log("Leaving full-screen mode.");
         document.getElementById("video-overlay").style.display = "block";
         isFullscreen = false;
         player.pause();
       }
     });
     document.addEventListener("webkitfullscreenchange", function(event) {
-      console.log("webkit fullscreen change", event);
       // The event object doesn't carry information about the fullscreen state of the browser,
       // but it is possible to retrieve it through the fullscreen API
       if (document.fullscreenElement) {
         isFullscreen = true;
         document.getElementById("video-overlay").style.display = "none";
-        console.log("ENTER FULLSCREEN");
       } else {
         document.getElementById("video-overlay").style.display = "block";
         isFullscreen = false;
-        console.log("EXITING FULLSCREEN");
+
         player.pause();
       }
     });
   });
+
   function playVideo() {
-    // player.enterFullscreen();
-    player.play();
+    player.enterFullscreen();
+    // This is weird, but safari won't play if it at the same time toggles into fullscreen mode.
+    setTimeout(function() {
+      player.play();
+    }, 300);
   }
 </script>
 
@@ -94,7 +84,6 @@
         <Icons icon="playbutton" />
       </div>
       <div class="pt-0 text-lg text-center text-white sm:pt-2">play video</div>
-      Issi: {player ? player.playing : null}
     </button>
   </div>
 </div>
@@ -118,9 +107,5 @@
   #container {
     width: 100%;
     max-width: 960px;
-  }
-  .video-overlay {
-    /* --tw-bg-opacity: 0.5;
-    background-color: rgba(30, 58, 138, var(--tw-bg-opacity)); */
   }
 </style>
