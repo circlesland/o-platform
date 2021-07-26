33622372d7d918d1ecaabcd197666b1904f04bb6
Thorsten Schau <letsgo@codeho.net>
2021-07-26 12:42:09 +0200

    topnav

--- shell/src/shared/atoms/TopNav.svelte
+++ shell/src/shared/atoms/TopNav.svelte
@@ -12,22 +12,31 @@
 
 <div class="fixed top-0 left-0 z-10 w-full">
   <div
-    class="grid w-full grid-cols-3 p-2 mx-auto text-white navbar bg-dark-dark"
+    class="grid w-full grid-cols-3 p-2 mx-auto text-white navbar bg-dark-dark justify-items-stretch"
   >
-    <div
-      class="justify-self-start"
-      class:col-span-2={runtimeDapp.title == 'Circles Land'}
-    >
+    <div class="justify-self-start">
       <img src="/logos/circles.png" class="w-8 h-8" alt="Circles Land" />
       <span class="ml-2 text-2xl uppercase font-heading text-light">
         {runtimeDapp.title}
       </span>
     </div>
-    {#if routable.title != runtimeDapp.title}
-      <div class="justify-self-center">
+
+    <div class="justify-self-center">
+      {#if routable.title != runtimeDapp.title}
         <span class="text-md">{routable.title}</span>
+      {/if}
+    </div>
+
+    <div class="col-start-3 pr-1 place-self-end justify-self-end">
+      <div class="self-center text-center avatar ">
+        <div class="w-8 h-8 rounded-full ring ring-primary">
+          <img
+            src="https://images.unsplash.com/photo-1493962333801-c5f3664c14f3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGhvdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="alt"
+          />
+        </div>
       </div>
-    {/if}
+    </div>
   </div>
 
 </div>
