8d90d79f8dc7a877e3b45e20fcf8648d4898c4f2
Thorsten Schau <letsgo@codeho.net>
2021-07-26 12:05:04 +0200

    Auto stash before rebase of "origin/20210726_newDappFrameStateMachine"

--- shell/src/dapps/o-onboarding/layouts/Layout.svelte
+++ shell/src/dapps/o-onboarding/layouts/Layout.svelte
@@ -1,248 +1,220 @@
 <script lang="ts">
-    import LeftDesktop from "./desktop/Left.svelte";
-    import Home from "../../o-homepage/pages/Home.svelte";
-    import LeftMobile from "./mobile/Left.svelte";
-    import RightDesktop from "./desktop/Right.svelte";
-    import RightMobile from "./mobile/Right.svelte";
-    import Text from "../views/Text.svelte";
-    import NavigationList from "../views/NavigationList.svelte";
-    import FilterList from "../views/FilterList.svelte";
-    import Center from "./Center.svelte";
-    import NextNav from "../../../shared/molecules/NextNav/NextNav.svelte";
-    import {NavigationManifest} from "@o-platform/o-interfaces/dist/navigationManifest";
-    import ListComponent from "../../../shared/molecules/NextNav/Components/List.svelte";
-    import ActionButtonComponent from "../../../shared/molecules/NextNav/Components/ActionButton.svelte";
-    import LinkComponent from "../../../shared/molecules/NextNav/Components/Link.svelte";
-    import {isMobile} from "../../../shared/functions/isMobile";
-    import {Layout} from "./layout";
+  import LeftDesktop from "./desktop/Left.svelte";
+  import Home from "../../o-homepage/pages/Home.svelte";
+  import LeftMobile from "./mobile/Left.svelte";
+  import RightDesktop from "./desktop/Right.svelte";
+  import RightMobile from "./mobile/Right.svelte";
+  import Text from "../views/Text.svelte";
+  import NavigationList from "../views/NavigationList.svelte";
+  import FilterList from "../views/FilterList.svelte";
+  import Center from "./Center.svelte";
+  import NextNav from "../../../shared/molecules/NextNav/NextNav.svelte";
+  import { NavigationManifest } from "@o-platform/o-interfaces/dist/navigationManifest";
+  import ListComponent from "../../../shared/molecules/NextNav/Components/List.svelte";
+  import ActionButtonComponent from "../../../shared/molecules/NextNav/Components/ActionButton.svelte";
+  import LinkComponent from "../../../shared/molecules/NextNav/Components/Link.svelte";
+  import { isMobile } from "../../../shared/functions/isMobile";
+  import { Layout } from "./layout";
 
-    // Import Swiper styles
-    import "swiper/swiper-bundle.css";
+  let dapp = "homepage:!";
 
-    import "swiper/components/navigation/navigation.min.css";
-    import "swiper/components/pagination/pagination.min.css";
+  let layout: Layout = {
+    main: {
+      component: Home,
+      params: {},
+    },
+    dialogs: {
+      left: {
+        isOpen: true,
+        component: NavigationList,
+        params: {},
+      },
+      center: {
+        isOpen: false,
+        component: Text,
+        params: { text: "CENTER" },
+      },
+      right: {
+        isOpen: false,
+        component: FilterList,
+        params: {},
+      },
+    },
+  };
 
-    // import Swiper core and required modules
-    import SwiperCore, {Pagination, Navigation} from "swiper/core";
-    import Pager from "../views/Pager.svelte";
-
-    // install Swiper modules
-    SwiperCore.use([Navigation, Pagination]);
-
-    let dapp = "homepage:!";
-
-    let layout: Layout = {
-        main: {
-            component: Home,
-            params: {}
+  let navigation: NavigationManifest = {
+    navPill: {
+      left: {
+        component: ListComponent,
+        props: {
+          icon: "list",
+          action: () => {
+            layout.dialogs.left.isOpen = !layout.dialogs.left.isOpen;
+          },
         },
-        dialogs: {
-            left: {
-                isOpen: false,
-                component: NavigationList,
-                params: {}
-            },
-            center: {
-                isOpen: false,
-                component: Text,
-                params: {text: "CENTER"}
-            },
-            right: {
-                isOpen: false,
-                component: FilterList,
-                params: {}
-            }
-        }
-    }
-
-    let navigation: NavigationManifest = {
-        navPill: {
-            left: {
-                component: ListComponent,
-                props: {
-                    icon: "list",
-                    action: () => {
-                        layout.dialogs.left.isOpen = !layout.dialogs.left.isOpen;
-                    },
-                },
-            },
-            center: {
-                component: ActionButtonComponent,
-                props: {
-                    icon: "logo",
-                    action: () => {
-                        layout.dialogs.center.isOpen = !layout.dialogs.center.isOpen;
-                    }
-                },
-            },
-            right: {
-                component: LinkComponent,
-                props: {
-                    icon: "home",
-                    action: () => {
-                        alert("Home sweet home")
-                    },
-                },
-            },
+      },
+      center: {
+        component: ActionButtonComponent,
+        props: {
+          icon: "logo",
+          action: () => {
+            layout.dialogs.center.isOpen = !layout.dialogs.center.isOpen;
+          },
         },
-        leftSlot: {
-            component: LinkComponent,
-            props: {
-                icon: "list",
-                action: () => layout.dialogs.left.isOpen = !layout.dialogs.left.isOpen
-            },
-        }
-    };
+      },
+      right: {
+        component: LinkComponent,
+        props: {
+          icon: "home",
+          action: () => {
+            alert("Home sweet home");
+          },
+        },
+      },
+    },
+    leftSlot: {
+      component: LinkComponent,
+      props: {
+        icon: "list",
+        action: () =>
+          (layout.dialogs.left.isOpen = !layout.dialogs.left.isOpen),
+      },
+    },
+  };
 
-    const sliderPages = [/*{
+  const sliderPages = [
+    /*{
         title: "Item 1"
     },{
         title: "Item 2"
-    }*/];
+    }*/
+  ];
 </script>
 
-{#if isMobile()}
-    {#if layout.dialogs.left.isOpen}
-        <LeftMobile on:click={() => layout.dialogs.left.isOpen = false}>
-            <svelte:component this={layout.dialogs.left.component}
-                              {...(layout.dialogs.left.params ? layout.dialogs.left.params : {})}
-                              on:clickedOutside={() => layout.dialogs.left.isOpen = false}
-                              on:clickedItem={() => layout.dialogs.left.isOpen = false}
-                              on:clickedClose={() => layout.dialogs.left.isOpen = false} />
-        </LeftMobile>
+<div class="absolute flex flex-row w-full overflow-auto">
+  <main class="relative z-30 w-full overflow-auto">
+    {#if sliderPages.length > 0}
+      <Pager pages={sliderPages} />
     {/if}
-    <main class="z-30 flex-1 overflow-y-auto">
-        {#if sliderPages.length > 0}
-            <Pager pages={sliderPages} />
+    <div
+      class="flex flex-row w-full mainContent"
+      class:mb-16={!layout.dialogs.center.isOpen && dapp === 'homepage:1'}
+      class:blur={layout.dialogs.center.isOpen}
+    >
+      <div class="fixed z-50">
+        {#if layout.dialogs.left.isOpen}
+          <LeftMobile on:click={() => (layout.dialogs.left.isOpen = false)}>
+            <svelte:component
+              this={layout.dialogs.left.component}
+              {...layout.dialogs.left.params ? layout.dialogs.left.params : {}}
+              on:clickedOutside={() => (layout.dialogs.left.isOpen = false)}
+              on:clickedItem={() => (layout.dialogs.left.isOpen = false)}
+              on:clickedClose={() => (layout.dialogs.left.isOpen = false)}
+            />
+          </LeftMobile>
+          <LeftDesktop>
+            <svelte:component
+              this={layout.dialogs.left.component}
+              {...layout.dialogs.left.params ? layout.dialogs.left.params : {}}
+              on:clickedOutside={() => (layout.dialogs.left.isOpen = false)}
+              on:clickedItem={() => (layout.dialogs.left.isOpen = false)}
+              on:clickedClose={() => (layout.dialogs.left.isOpen = false)}
+            />
+          </LeftDesktop>
         {/if}
-        <div
-                class="absolute top-0 w-full mainContent"
-                class:mb-16={!layout.dialogs.center.isOpen && dapp === "homepage:1"}
-                class:blur={layout.dialogs.center.isOpen}
-        >
-            <svelte:component this={layout.main.component}
-                              {...(layout.main.params ? layout.main.params : {})} />
-        </div>
-    </main>
-
-    {#if layout.dialogs.right.isOpen}
-        <RightMobile on:click={() => layout.dialogs.right.isOpen = false}>
-            <svelte:component this={layout.dialogs.right.component}
-                              {...(layout.dialogs.right.params ? layout.dialogs.right.params : {})}
-                              on:clickedOutside={() => layout.dialogs.right.isOpen = false}
-                              on:clickedItem={() => layout.dialogs.right.isOpen = false}
-                              on:clickedClose={() => layout.dialogs.right.isOpen = false} />
-        </RightMobile>
-    {/if}
-{:else}
-    <div class="absolute flex flex-row w-full overflow-auto">
-        <main class="relative z-30 w-full overflow-auto">
-            {#if sliderPages.length > 0}
-                <Pager pages={sliderPages} />
-            {/if}
-            <div
-                    class="flex flex-row w-full mainContent"
-                    class:mb-16={!layout.dialogs.center.isOpen && dapp === "homepage:1"}
-                    class:blur={layout.dialogs.center.isOpen}
-            >
-                <div class="fixed">
-                    {#if layout.dialogs.left.isOpen}
-                        <LeftDesktop>
-                            <svelte:component this={layout.dialogs.left.component}
-                                              {...(layout.dialogs.left.params ? layout.dialogs.left.params : {})}
-                                              on:clickedOutside={() => layout.dialogs.left.isOpen = false}
-                                              on:clickedItem={() => layout.dialogs.left.isOpen = false}
-                                              on:clickedClose={() => layout.dialogs.left.isOpen = false}/>
-                        </LeftDesktop>
-                    {/if}
-                </div>
-                <div class="flex-grow">
-                    <svelte:component this={layout.main.component}
-                                      {...(layout.main.params ? layout.main.params : {})} />
-                </div>
-                <div>
-                    {#if layout.dialogs.right.isOpen}
-                        <RightDesktop>
-                            <svelte:component this={layout.dialogs.right.component}
-                                              {...(layout.dialogs.right.params ? layout.dialogs.right.params : {})}
-                                              on:clickedOutside={() => layout.dialogs.right.isOpen = false}
-                                              on:clickedItem={() => layout.dialogs.right.isOpen = false}
-                                              on:clickedClose={() => layout.dialogs.right.isOpen = false} />
-                        </RightDesktop>
-                    {/if}
-                </div>
-            </div>
-        </main>
-
+      </div>
+      <div class="flex-grow">
+        <svelte:component
+          this={layout.main.component}
+          {...layout.main.params ? layout.main.params : {}}
+        />
+      </div>
+      <div>
+        {#if layout.dialogs.right.isOpen}
+          <RightDesktop>
+            <svelte:component
+              this={layout.dialogs.right.component}
+              {...layout.dialogs.right.params ? layout.dialogs.right.params : {}}
+              on:clickedOutside={() => (layout.dialogs.right.isOpen = false)}
+              on:clickedItem={() => (layout.dialogs.right.isOpen = false)}
+              on:clickedClose={() => (layout.dialogs.right.isOpen = false)}
+            />
+          </RightDesktop>
+        {/if}
+      </div>
     </div>
-{/if}
+  </main>
+</div>
 
-<NextNav navigation={navigation} />
+<NextNav {navigation} />
 
 {#if layout.dialogs.center.isOpen}
-    <Center blur="true">
-        <svelte:component this={layout.dialogs.center.component} {...(layout.dialogs.center.params ? layout.dialogs.center.params : {})} />
-    </Center>
+  <Center blur="true">
+    <svelte:component
+      this={layout.dialogs.center.component}
+      {...layout.dialogs.center.params ? layout.dialogs.center.params : {}}
+    />
+  </Center>
 {/if}
 
 {#if !isMobile()}
-    <style>
-        nav.carousel:hover {
-            @apply cursor-default;
-        }
+  <style>
+    nav.carousel:hover {
+      @apply cursor-default;
+    }
 
-        /* Hide the radio button */
-        nav.carousel input[type="radio"] {
-            display: none;
-        }
-    </style>
+    /* Hide the radio button */
+    nav.carousel input[type="radio"] {
+      display: none;
+    }
+  </style>
 {/if}
 
 <style>
-    .tab:hover,
-    .tab:focus,
-    .tab:active {
-        @apply text-light;
-    }
+  .tab:hover,
+  .tab:focus,
+  .tab:active {
+    @apply text-light;
+  }
 
-    .tab:hover {
-        @apply text-base;
-    }
+  .tab:hover {
+    @apply text-base;
+  }
 
-    .isOpen {
-        background: transparent;
-        border: none;
-    }
+  .isOpen {
+    background: transparent;
+    border: none;
+  }
 
-    /* Background Blurring for firefox and other non supportive browsers */
-    @supports not (
+  /* Background Blurring for firefox and other non supportive browsers */
+  @supports not (
     (backdrop-filter: blur(4px)) or (-webkit-backdrop-filter: blur(4px))
   ) {
-        .blur {
-            filter: blur(4px);
-            -webkit-transition: all 0.35s ease-in-out;
-            -moz-transition: all 0.35s ease-in-out;
-            transition: all 0.35s ease-in-out;
-        }
-
-        /* Firefox fix for sticky bottom prev-sibling height */
-        main {
-            padding-bottom: 4rem;
-        }
+    .blur {
+      filter: blur(4px);
+      -webkit-transition: all 0.35s ease-in-out;
+      -moz-transition: all 0.35s ease-in-out;
+      transition: all 0.35s ease-in-out;
     }
 
-    .joinnowbutton {
-        transform: translate(-50%, 0) !important;
-        animation: none !important;
+    /* Firefox fix for sticky bottom prev-sibling height */
+    main {
+      padding-bottom: 4rem;
     }
+  }
 
-    .joinnowbutton:active:focus,
-    .joinnowbutton:active:hover {
-        transform: translate(-50%, 0);
-        animation: none !important;
-    }
+  .joinnowbutton {
+    transform: translate(-50%, 0) !important;
+    animation: none !important;
+  }
 
-    /* .mainContent {
+  .joinnowbutton:active:focus,
+  .joinnowbutton:active:hover {
+    transform: translate(-50%, 0);
+    animation: none !important;
+  }
+
+  /* .mainContent {
       --tw-text-opacity: 1;
       background-image: linear-gradient(
         180deg,
@@ -252,65 +224,33 @@
       );
     } */
 
-    .swiper-container {
-        width: 100%;
-        height: 100%;
-    }
+  nav.carousel:hover {
+    @apply cursor-default;
+  }
 
-    .swiper-slide {
-        text-align: center;
-        font-size: 18px;
-        background: #fff;
+  /* Hide the radio button */
+  nav.carousel input[type="radio"] {
+    display: none;
+  }
 
-        /* Center slide text vertically */
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: -webkit-flex;
-        display: flex;
-        -webkit-box-pack: center;
-        -ms-flex-pack: center;
-        -webkit-justify-content: center;
-        justify-content: center;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        -webkit-align-items: center;
-        align-items: center;
-    }
-
-    .swiper-slide img {
-        display: block;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-
-    nav.carousel:hover {
-        @apply cursor-default;
-    }
-
-    /* Hide the radio button */
-    nav.carousel input[type="radio"] {
-        display: none;
-    }
-
-    /* All styling takes place on the label element */
-    nav.carousel label {
-        @apply inline-block;
-        @apply bg-dark;
-        @apply overflow-hidden;
-        @apply rounded-full;
-        @apply w-2;
-        @apply h-2;
-        text-indent: -999px;
-        /* box-shadow: inset 0 1px 1px 0 #999; */
-    }
-    nav.carousel label:hover {
-        @apply bg-dark;
-        @apply cursor-pointer;
-        /* box-shadow: inset 0 1px 1px 0 #777; */
-    }
-    nav.carousel input:checked + label {
-        @apply bg-light;
-        /* box-shadow: inset 0 0 1px 1px #087dc0; */
-    }
+  /* All styling takes place on the label element */
+  nav.carousel label {
+    @apply inline-block;
+    @apply bg-dark;
+    @apply overflow-hidden;
+    @apply rounded-full;
+    @apply w-2;
+    @apply h-2;
+    text-indent: -999px;
+    /* box-shadow: inset 0 1px 1px 0 #999; */
+  }
+  nav.carousel label:hover {
+    @apply bg-dark;
+    @apply cursor-pointer;
+    /* box-shadow: inset 0 1px 1px 0 #777; */
+  }
+  nav.carousel input:checked + label {
+    @apply bg-light;
+    /* box-shadow: inset 0 0 1px 1px #087dc0; */
+  }
 </style>
--- shell/src/dapps/o-onboarding/views/NavigationList.svelte
+++ shell/src/dapps/o-onboarding/views/NavigationList.svelte
@@ -1,45 +1,63 @@
 <script>
-    import Icons from "../../../shared/molecules/Icons.svelte";
-    import { clickOutside } from "src/shared/functions/clickOutside.ts";
-    import {createEventDispatcher} from "svelte";
-    import {isMobile} from "../../../shared/functions/isMobile";
+  import Icons from "../../../shared/molecules/Icons.svelte";
+  import { clickOutside } from "src/shared/functions/clickOutside.ts";
+  import { createEventDispatcher } from "svelte";
+  import { isMobile } from "../../../shared/functions/isMobile";
+  import LinkPill from "src/shared/atoms/LinkPill.svelte";
 
-    const navigation = [
-        {extern:true, url:"https://circles.land", icon: "home", title: "Link 1"},
-        {extern:true, url:"https://circles.land", icon: "home", title: "Link 2"},
-        {extern:true, url:"https://circles.land", icon: "home", title: "Link 3"},
-        {extern:true, url:"https://circles.land", icon: "home", title: "Link 4"}
-    ];
+  const navigation = [
+    {
+      extern: true,
+      url: "https://circles.land",
+      icon: "home",
+      title: "Link 1",
+    },
+    {
+      extern: true,
+      url: "https://circles.land",
+      icon: "home",
+      title: "Link 2",
+    },
+    {
+      extern: true,
+      url: "https://circles.land",
+      icon: "home",
+      title: "Link 3",
+    },
+    {
+      extern: true,
+      url: "https://circles.land",
+      icon: "home",
+      title: "Link 4",
+    },
+  ];
 
-    const eventDispatcher = createEventDispatcher();
+  const eventDispatcher = createEventDispatcher();
 </script>
+
 <div
-        class="z-10 flex flex-col flex-1 text-white bg-dark"
-        use:clickOutside
-        on:click_outside={() => eventDispatcher("clickedOutside")}
+  class="z-10 flex flex-col flex-1"
+  use:clickOutside
+  on:click_outside={() => eventDispatcher('clickedOutside')}
 >
-    <nav class="flex flex-col flex-1 w-64 p-4 mt-4" />
-    <div class="relative flex-shrink-0 w-64 p-6 pt-4 pb-8 space-y-6">
-        {#if navigation}
-            {#each navigation as navItem}
-                <a
-                        href={navItem.extern ? navItem.url : '/#/' + navItem.url}
-                        class="flex content-center justify-start space-x-2"
-                        target={navItem.extern ? '_blank' : '_self'}
-                        on:click={() => eventDispatcher("clickedItem")}
-                >
-                    <Icons icon={navItem.icon} />
-                    <div>{navItem.title}</div>
-                </a>
-            {/each}
-        {/if}
-    </div>
+  <nav class="flex flex-col flex-1 p-4 mt-4 w-52" />
+  <div class="relative flex-shrink-0 p-4 pt-4 pb-20 space-y-2 w-52">
+    {#if navigation}
+      {#each navigation as navItem}
+        <LinkPill
+          props={{ icon: navItem.icon, text: navItem.title, link: navItem.url, isActive: navItem.isActive }}
+        />
+      {/each}
+    {/if}
+  </div>
 </div>
-{#if !isMobile()}
-    <div
-            class="fixed z-50 flex justify-center flex-shrink-0 w-12 h-12 px-3 py-4 ml-4 bg-white rounded-full cursor-pointer bottom-6 left-72"
-            on:click={() => eventDispatcher("clickedClose")}
-    >
-        <Icons icon="buttonleftarrow" />
-    </div>
-{/if}
+<div
+  class="fixed z-50 flex justify-center flex-shrink-0 w-12 h-12 px-3 py-4 ml-4 bg-white rounded-full cursor-pointer mobile-view bottom-6 left-72"
+  on:click={() => eventDispatcher('clickedClose')}
+>
+  <Icons icon="buttonleftarrow" />
+</div>
+
+<style>
+
+</style>
