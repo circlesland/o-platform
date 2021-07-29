ab48d79c6d15428c31b494e6c89f161fee21f3f1
Thorsten <letsgo@codeho.net>
2021-07-29 18:23:04 +0200

    click outside left & right

--- shell/src/dapps/o-onboarding/layouts/desktop/Left.svelte
+++ shell/src/dapps/o-onboarding/layouts/desktop/Left.svelte
@@ -1,12 +1,22 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { clickOutside } from "src/shared/functions/clickOutside";
+
+  const eventDispatcher = createEventDispatcher();
+</script>
+
 <aside
-  class="z-50 flex flex-col flex-1 flex-shrink-0 w-64 h-screen mt-12 desktop-view"
->
+  class="z-50 flex flex-col flex-1 flex-shrink-0 w-64 h-screen mt-12 desktop-view">
   <div class="">
     <!-- Sidebar -->
-    <div class="fixed inset-y-0 z-10 flex w-72 sidebar">
+    <div
+      class="fixed inset-y-0 z-10 flex w-72 sidebar"
+      use:clickOutside
+      on:click_outside="{() => eventDispatcher('clickedOutside')}">
+
       <!-- Sidebar content -->
       <div class="z-10 flex flex-col flex-1 ">
-        <nav class="flex flex-col flex-1 w-64 p-4 mt-4" />
+        <nav class="flex flex-col flex-1 w-64 p-4 mt-4"></nav>
         <div class="relative flex-shrink-0 w-64 p-4 pt-4 pb-20 space-y-2">
           <slot />
         </div>
