c921f5f7416b14a7ddae0d11256d698e987b2597
Thorsten Schau <letsgo@codeho.net>
2021-08-03 11:02:44 +0200

    tinkering with marketplace offers

--- shell/src/dapps/o-marketplace/pages/Home.svelte
+++ shell/src/dapps/o-marketplace/pages/Home.svelte
@@ -97,7 +97,7 @@
 
 <SimpleHeader {runtimeDapp} {routable} />
 
-<div class="px-4 mx-auto -mt-3 md:w-2/3 xl:w-1/2">
+<div class="px-4 mx-auto -mt-3 lg:w-2/3 xl:w-1/2">
   {#if isLoading}
     <section class="flex items-center justify-center mx-4 mb-2 ">
       <div class="flex items-center w-full p-4 space-x-2 bg-white shadow ">
