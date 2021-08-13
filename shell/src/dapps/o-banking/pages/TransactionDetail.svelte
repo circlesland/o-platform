0fe62f462664c6f1a32a23ae35df195c3ca94796
Thorsten Schau <letsgo@codeho.net>
2021-08-13 10:45:55 +0200

    new Transaction Detail Design

--- shell/src/dapps/o-banking/pages/TransactionDetail.svelte
+++ shell/src/dapps/o-banking/pages/TransactionDetail.svelte
@@ -6,12 +6,11 @@
   import { RpcGateway } from "@o-platform/o-circles/dist/rpcGateway";
   import { AvataarGenerator } from "../../../shared/avataarGenerator";
   import { Transfer } from "../data/circles/types";
-
+  import Web3 from "web3";
   import Icons from "../../../shared/molecules/Icons.svelte";
   import { push } from "svelte-spa-router";
   import { RuntimeDapp } from "@o-platform/o-interfaces/dist/runtimeDapp";
   import { Routable } from "@o-platform/o-interfaces/dist/routable";
-  import PaymentPath from "../../../shared/molecules/PaymentPath.svelte";
   import CirclesTransferGraph from "../../../shared/pathfinder/CirclesTransferGraph.svelte";
 
   export let _id: string;
@@ -23,6 +22,7 @@
   let pictureUrl: string;
   let displayName: string;
   let displayableFromName: string;
+  let displayableToName: string;
   let classes: string;
   let message: String;
   let amountInWei: string;
@@ -40,6 +40,9 @@
         ? transfer.fromProfile.displayName
         : transfer.from;
 
+      displayableToName = transfer.toProfile
+        ? transfer.toProfile.displayName
+        : transfer.to;
       displayName =
         transfer.direction === "in"
           ? transfer.fromProfile
@@ -58,10 +61,7 @@
           ? transfer.toProfile.avatarUrl
           : undefined;
 
-      classes =
-        transfer.direction === "in"
-          ? "transactionpositive"
-          : "transactionnegative";
+      classes = transfer.direction === "in" ? "text-success" : "text-alert";
 
       displayableFromName =
         displayableFromName === "0x0000000000000000000000000000000000000000"
@@ -103,75 +103,91 @@
   }
 </script>
 
+<!-- 
 <BankingDetailHeader
   {runtimeDapp}
   {routable}
   amount="{transfer ? transfer.amount : 0}"
-  {classes} />
-<div class="px-6 pb-6">
+  {classes} /> -->
+<div class="p-5">
   {#if transfer}
-    <section class="flex items-center justify-center pt-10 pb-2 text-dark">
-      <div class="flex flex-col w-full space-y-2 ">
-        <div class="flex flex-row justify-center w-full space-x-2 sm:space-x-6">
-          <div
-            class="flex flex-col cursor-pointer"
-            on:click="{() => openDetail(transfer.from)}">
-            <div class="avatar">
-              <div class="w-24 h-24 m-auto rounded-full">
-                <img
-                  alt="{displayableFromName}"
-                  src="{transfer.fromProfile && transfer.fromProfile.avatarUrl ? transfer.fromProfile.avatarUrl : transfer.from.startsWith('0x000') ? '/images/common/circles.png' : pictureUrl}" />
-              </div>
-            </div>
-            <div class="block mt-2 text-center">{displayableFromName}</div>
-          </div>
+    <div
+      class="flex flex-col items-center self-center w-full m-auto space-y-4 text-center justify-self-center">
+      <div class="w-full text-center">
+        <h1 class="text-3xl uppercase font-heading">
+          {transfer.direction === 'in' ? 'received' : 'sent'}
+        </h1>
+      </div>
+      <div>
+        <span class="inline-block text-6xl font-heading {classes}">
+          {#if transfer.direction === 'in'}
+            +{Number.parseFloat(Web3.utils.fromWei(transfer ? transfer.amount : '0', 'ether')).toFixed(2)}
+          {:else}
+            -{Number.parseFloat(Web3.utils.fromWei(transfer ? transfer.amount : '0', 'ether')).toFixed(2)}
+          {/if}
 
-          <div class="self-center text-xl text-dark-lightest">
-            <Icons icon="rightarrow" />
-          </div>
-          <div
-            class="flex flex-col cursor-pointer"
-            on:click="{() => openDetail(transfer.to)}">
-            <div class="avatar">
-              <div class="w-24 h-24 m-auto rounded-full">
-                <img
-                  alt="{transfer.toProfile ? transfer.toProfile.displayName : transfer.to}"
-                  src="{transfer.toProfile && transfer.toProfile.avatarUrl ? transfer.toProfile.avatarUrl : pictureUrl}" />
-              </div>
-            </div>
-            <div class="block mt-2 text-center">
-              {transfer.toProfile ? transfer.toProfile.displayName : transfer.to}
-            </div>
-          </div>
+          <svg
+            class="inline w-10 h-10 -ml-2"
+            viewBox="0 0 229 255"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M118.5 237C150.437 237 179.424 224.366 200.734 203.822C209.904
+              197.627 215.933 187.136 215.933 175.236C215.933 156.198 200.499
+              140.764 181.461 140.764C170.572 140.764 160.863 145.812 154.545
+              153.695L154.457 153.627C145.313 163.112 132.476 169.012 118.261
+              169.012C90.4957 169.012 67.9879 146.504 67.9879 118.739C67.9879
+              90.9745 90.4957 68.4667 118.261 68.4667C132.339 68.4667 145.067
+              74.254 154.193 83.5795L154.29 83.5037C160.581 90.2293 169.535
+              94.4328 179.471 94.4328C198.51 94.4328 213.944 78.9988 213.944
+              59.9601C213.944 48.1884 208.043 37.7949 199.039 31.5755C177.899
+              11.9794 149.599 0 118.5 0C53.0543 0 0 53.0543 0 118.5C0 183.946
+              53.0543 237 118.5 237Z"
+              fill="currentColor"></path>
+            <ellipse
+              cx="118.979"
+              cy="118.739"
+              rx="26.5727"
+              ry="26.3333"
+              fill="currentColor"></ellipse>
+          </svg>
+        </span>
+      </div>
+      <div class="avatar rounded-corners-gradient-borders">
+        <div class="m-auto bg-white rounded-full w-36 h-36">
+          {#if transfer.direction === 'in'}
+            <img
+              alt="{displayableFromName}"
+              src="{transfer.fromProfile && transfer.fromProfile.avatarUrl ? transfer.fromProfile.avatarUrl : transfer.from.startsWith('0x000') ? '/images/common/circles.png' : pictureUrl}" />
+          {:else}
+            <img
+              alt="{transfer.toProfile ? transfer.toProfile.displayName : transfer.to}"
+              src="{transfer.toProfile && transfer.toProfile.avatarUrl ? transfer.toProfile.avatarUrl : pictureUrl}" />
+          {/if}
         </div>
       </div>
-    </section>
-
-    <section class="justify-center mt-4 mb-2">
-      <div class="flex flex-col w-full space-y-1">
-        <div class="text-left text-2xs text-dark-lightest">Message</div>
-
-        <div class="flex items-center w-full ">
-          <div class="text-2xl leading-tight text-left font-heading">
-            {message ? message : 'No Message'}
-          </div>
-        </div>
+      <div>
+        {#if transfer.direction === 'in'}
+          <span class="mt-4 text-xl">from {displayableFromName}</span>
+        {:else}
+          <span class="mt-4 text-xl">to {displayableToName}</span>
+        {/if}
       </div>
-    </section>
+      <div class="text-dark-lightest">{message ? message : 'No Message'}</div>
 
-    <section class="justify-center mt-4 mb-2">
       <div class="flex flex-col w-full space-y-1">
-        <div class="mb-1 text-left text-2xs text-dark-lightest">Amount</div>
+        <div class="mb-1 text-left text-2xs text-dark-lightest">
+          Payment Path
+        </div>
 
         <div class="flex items-center w-full">
-          <div class="text-left ">
-            {amountInWei} {amountInWei > 1 ? ' Cirlces' : ' Circle'}
-          </div>
+          <!-- <CirclesTransferGraph transfers="{[transfer]}" /> -->
+          Coming soon...
         </div>
       </div>
-    </section>
 
-    <section class="justify-center mt-4 mb-2">
       <div class="flex flex-col w-full space-y-1">
         <div class="mb-1 text-left text-2xs text-dark-lightest">Date</div>
 
@@ -183,21 +199,27 @@
           </div>
         </div>
       </div>
-    </section>
 
-    <section class="justify-center mt-4 mb-2">
+      <div class="flex flex-col w-full space-y-1">
+        <div class="mb-1 text-left text-2xs text-dark-lightest">Amount</div>
+
+        <div class="flex items-center w-full">
+          <div class="text-left ">
+            {amountInWei} {amountInWei > 1 ? ' Cirlces' : ' Circle'}
+          </div>
+        </div>
+      </div>
+
       <div class="flex flex-col w-full space-y-1">
         <div class="mb-1 text-left text-2xs text-dark-lightest">From</div>
 
         <div class="flex items-center w-full">
           <div class="text-left break-all">
-            {transfer.fromProfile ? `${transfer.from}` : ''}
+            {transfer.fromProfile ? `${transfer.from}` : 'Circlesland'}
           </div>
         </div>
       </div>
-    </section>
 
-    <section class="justify-center mt-4 mb-2">
       <div class="flex flex-col w-full space-y-1">
         <div class="mb-1 text-left text-2xs text-dark-lightest">To</div>
 
@@ -207,9 +229,7 @@
           </div>
         </div>
       </div>
-    </section>
 
-    <section class="justify-center mt-4 mb-2">
       <div class="flex flex-col w-full space-y-1">
         <div class="mb-1 text-left text-2xs text-dark-lightest">Block</div>
 
@@ -217,10 +237,8 @@
           <div class="text-left break-all">{transfer.firstBlock}</div>
         </div>
       </div>
-    </section>
 
-    {#if transfer.transactionHash}
-      <section class="justify-center mt-4 mb-2">
+      {#if transfer.transactionHash}
         <div class="flex flex-col w-full space-y-1">
           <div class="mb-1 text-left text-2xs text-dark-lightest">
             Transaction Hash
@@ -232,18 +250,16 @@
             </div>
           </div>
         </div>
-      </section>
-    {/if}
+      {/if}
 
-    {#if false}
-      <section class="justify-center mt-4 mb-2">
+      {#if false}
         <div class="flex flex-col w-full space-y-1">
           <div class="mb-1 text-left text-2xs text-dark-lightest">
             Payment path
           </div>
           <CirclesTransferGraph transfers="{path.transfers}" />
         </div>
-      </section>
-    {/if}
+      {/if}
+    </div>
   {/if}
 </div>
