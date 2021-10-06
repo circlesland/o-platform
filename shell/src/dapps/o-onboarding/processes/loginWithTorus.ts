1ad0b3341dec9ebf76525cf24fc12095d6bc0618
jaensen <daniel@djanz.de>
2021-10-06 21:12:43 +0200

    rolled back from the CustomAuth to the default OpenAuth implementation of tor.us

--- shell/src/dapps/o-onboarding/processes/loginWithTorus.ts
+++ shell/src/dapps/o-onboarding/processes/loginWithTorus.ts
@@ -6,14 +6,14 @@ import { promptChoice } from "../../o-passport/processes/identify/prompts/prompt
 import ButtonStackSelector from "@o-platform/o-editors/src/ButtonStackSelector.svelte";
 import NumberEditor from "../../../../../packages/o-editors/src/NumberEditor.svelte";
 import * as yup from "yup";
-import { prompt } from "@o-platform/o-process/dist/states/prompt";
-import { KeyManager } from "../../o-passport/data/keyManager";
-import { RpcGateway } from "@o-platform/o-circles/dist/rpcGateway";
+import {prompt} from "@o-platform/o-process/dist/states/prompt";
+import {KeyManager} from "../../o-passport/data/keyManager";
+import {RpcGateway} from "@o-platform/o-circles/dist/rpcGateway";
 import HtmlViewer from "../../../../../packages/o-editors/src/HtmlViewer.svelte";
-import { PlatformEvent } from "@o-platform/o-events/dist/platformEvent";
-import { show } from "@o-platform/o-process/dist/actions/show";
+import {PlatformEvent} from "@o-platform/o-events/dist/platformEvent";
+import {show} from "@o-platform/o-process/dist/actions/show";
 import ErrorView from "../../../shared/atoms/Error.svelte";
-import { getOpenLogin } from "../../../shared/openLogin";
+import {getOpenLogin} from "../../../shared/openLogin";
 
 export type LoginWithTorusContextData = {
   chooseFlow?: {
@@ -21,11 +21,11 @@ export type LoginWithTorusContextData = {
     label: string;
   };
   userInfo?: any;
-  privateKey?: string;
-  encryptionPin?: string;
-  decryptionPin?: string;
-  accountAddress?: string;
-  successAction?: (data: LoginWithTorusContextData) => void;
+  privateKey?:string;
+  encryptionPin?:string;
+  decryptionPin?:string;
+  accountAddress?:string;
+  successAction?: (data:LoginWithTorusContextData) => void;
 };
 
 export type LoginWithTorusContext = ProcessContext<LoginWithTorusContextData>;
@@ -112,6 +112,7 @@ const processDefinition = (processId: string) =>
           id: "google",
           invoke: {
             src: async (context) => {
+              /*
               const openLogin = await getOpenLogin();
               const authResult = await openLogin.triggerLogin({
                 typeOfLogin: "google",
@@ -125,105 +126,97 @@ const processDefinition = (processId: string) =>
                 privateKey: context.data.privateKey,
                 userInfo: context.data.userInfo,
               };
-              /*
-            const privateKey = await openLogin.login({
-              loginProvider: "google",
-              extraLoginOptions: {
-                prompt: "consent",
-                display: "touch"
-              }
-            });
-            const userInfo = await openLogin.getUserInfo();
-            return {
-              privateKey: privateKey,
-              userInfo: userInfo
-            };
-             */
-            },
-            onDone: {
-              actions: "assignPrivateKeyAndUserInfoToContext",
-              target: "#enterEncryptionPin",
-            },
-            onError: {
-              target: "#chooseFlow",
-            },
+               */
+              const openLogin = await getOpenLogin();
+              const privateKey = await openLogin.login({
+                loginProvider: "google",
+                extraLoginOptions: {
+                  prompt: "consent",
+                  display: "touch"
+                }
+              });
+              const userInfo = await openLogin.getUserInfo();
+              return {
+                privateKey: privateKey.privKey,
+                userInfo: userInfo
+              };
           },
-        },
-        apple: {
-          id: "apple",
-          invoke: {
-            src: async (context) => {
-              /*
+          onDone: {
+            actions: "assignPrivateKeyAndUserInfoToContext",
+            target: "#enterEncryptionPin"
+          },
+          onError: {
+            target: "#chooseFlow",
+          }
+        }
+      },
+      apple: {
+        id: "apple",
+        invoke: {
+          src: async (context) => {
             const openLogin = await getOpenLogin();
             const privateKey = await openLogin.login({
-              loginProvider: "apple"
+              loginProvider: "apple",
             });
             return {
-              privateKey: privateKey,
-              userInfo: await openLogin.getUserInfo()
+              privateKey: privateKey.privKey,
+              userInfo: await openLogin.getUserInfo(),
             };
-
-             */
-            },
-            onDone: {
-              actions: "assignPrivateKeyAndUserInfoToContext",
-              target: "#enterEncryptionPin",
-            },
-            onError: {
-              target: "#chooseFlow",
-            },
           },
-        },
-        github: {
-          id: "github",
-          invoke: {
-            src: async (context) => {
-              /*
+          onDone: {
+            actions: "assignPrivateKeyAndUserInfoToContext",
+            target: "#enterEncryptionPin"
+          },
+          onError: {
+            target: "#chooseFlow",
+          }
+        }
+      },
+      github: {
+        id: "github",
+        invoke: {
+          src: async (context) => {
             const openLogin = await getOpenLogin();
             const privateKey = await openLogin.login({
-              loginProvider: "github"
+              loginProvider: "github",
             });
             return {
-              privateKey: privateKey,
-              userInfo: await openLogin.getUserInfo()
+              privateKey: privateKey.privKey,
+              userInfo: await openLogin.getUserInfo(),
             };
-
-             */
-            },
-            onDone: {
-              actions: "assignPrivateKeyAndUserInfoToContext",
-              target: "#enterEncryptionPin",
-            },
-            onError: {
-              target: "#chooseFlow",
-            },
           },
-        },
-        facebook: {
-          id: "facebook",
-          invoke: {
-            src: async (context) => {
-              /*
+          onDone: {
+            actions: "assignPrivateKeyAndUserInfoToContext",
+            target: "#enterEncryptionPin"
+          },
+          onError: {
+            target: "#chooseFlow",
+          }
+        }
+      },
+        /*
+      facebook: {
+        id: "facebook",
+        invoke: {
+          src: async (context) => {
             const openLogin = await getOpenLogin();
             const privateKey = await openLogin.login({
-              loginProvider: "facebook"
+              loginProvider: "facebook",
             });
             return {
               privateKey: privateKey,
-              userInfo: await openLogin.getUserInfo()
+              userInfo: await openLogin.getUserInfo(),
             };
-
-             */
-            },
-            onDone: {
-              actions: "assignPrivateKeyAndUserInfoToContext",
-              target: "#enterEncryptionPin",
-            },
-            onError: {
-              target: "#chooseFlow",
-            },
           },
-        } /*
+          onDone: {
+            actions: "assignPrivateKeyAndUserInfoToContext",
+            target: "#enterEncryptionPin"
+          },
+          onError: {
+            target: "#chooseFlow",
+          }
+        }
+      },
       email: {
         id: "email",
         invoke: {
@@ -242,157 +235,133 @@ const processDefinition = (processId: string) =>
             target: "#enterEncryptionPin"
           }
         }
-      },*/,
-        enterEncryptionPin: prompt<LoginWithTorusContext, any>({
-          id: "enterEncryptionPin",
-          field: "encryptionPin",
-          component: NumberEditor,
-          isSensitive: true,
-          params: {
-            view: {
-              title: "Please enter a pin",
-              description:
-                "The pin will be used to encrypt your private key on your device.",
-              placeholder: "Enter Pin",
-              submitButtonText: "Store private key on this device",
-            },
-          },
-          dataSchema: yup
-            .string()
-            .required("Please enter a pin to protect your private key."),
-          navigation: {
-            next: "#storeKey",
-          },
-        }),
-        enterDecryptionPin: prompt<LoginWithTorusContext, any>({
-          id: "enterDecryptionPin",
-          field: "decryptionPin",
-          component: NumberEditor,
-          isSensitive: true,
-          params: {
-            view: {
-              title: "Please your pin",
-              description:
-                "The pin will be used to decrypt your private key on your device.",
-              placeholder: "Enter Pin",
-              submitButtonText: "Unlock",
-            },
-          },
-          dataSchema: yup
-            .string()
-            .required(
-              "Please enter a encryptingPin to protect your private key."
-            ),
-          navigation: {
-            next: "#unlockKey",
-          },
-        }),
-        unlockKey: {
-          id: "unlockKey",
-          invoke: {
-            src: async (context) => {
-              const km = new KeyManager(null);
-              await km.load();
-              let privateKey: string | null = null;
-              try {
-                privateKey = await km.getKey(
-                  context.data.accountAddress,
-                  context.data.decryptionPin
-                );
-              } catch (e) {
-                context.messages[
-                  "decryptionPin"
-                ] = `Couldn't decrypt your key: ${e.message}`;
-                throw e;
-              }
-
-              if (!privateKey || privateKey == "") {
-                delete context.data.decryptionPin;
-                delete context.data.privateKey;
-                context.messages["decryptionPin"] =
-                  "Couldn't decrypt your key. Have you entered the correct pin?";
-                throw new Error(context.messages["decryptionPin"]);
-              }
-
-              sessionStorage.setItem("circlesKey", privateKey);
-
-              delete context.data.privateKey;
-              delete context.data.decryptionPin;
-            },
-            onDone: "#success",
-            onError: {
-              target: "#enterDecryptionPin",
-            },
-          },
+      },*/
+      enterEncryptionPin: prompt<LoginWithTorusContext, any>({
+        id: "enterEncryptionPin",
+        field: "encryptionPin",
+        component: NumberEditor,
+        isSensitive: true,
+        params: {
+          view: {
+            title: "Please enter a pin",
+            description: "The pin will be used to encrypt your private key on your device.",
+            placeholder: "Enter Pin",
+            submitButtonText: "Store private key on this device",
+          }
         },
-        storeKey: {
-          id: "storeKey",
-          invoke: {
-            src: async (context) => {
-              const km = new KeyManager(null);
-              const account = RpcGateway.get().eth.accounts.privateKeyToAccount(
-                context.data.privateKey
-              );
-
-              await km.addEoa(
-                account.address,
-                account.privateKey,
-                context.data.encryptionPin,
-                "torus"
-              );
-
-              context.data.accountAddress = account.address;
-              sessionStorage.setItem("circlesKey", account.privateKey);
-
-              delete context.data.privateKey;
-              delete context.data.encryptionPin;
-            },
-            onDone: "#success",
-            onError: {
-              actions: (context, event) => {
-                window.o.lastError = event.data;
-              },
-              target: "#showError",
-            },
-          },
+        dataSchema: yup.string().required("Please enter a pin to protect your private key."),
+        navigation: {
+          next: "#storeKey",
+        }
+      }),
+      enterDecryptionPin: prompt<LoginWithTorusContext, any>({
+        id: "enterDecryptionPin",
+        field: "decryptionPin",
+        component: NumberEditor,
+        isSensitive: true,
+        params: {
+          view: {
+            title: "Please your pin",
+            description: "The pin will be used to decrypt your private key on your device.",
+            placeholder: "Enter Pin",
+            submitButtonText: "Unlock",
+          }
         },
-        showError: {
-          id: "showError",
-          entry: show({
-            // TODO: fix <any> cast
-            component: ErrorView,
-            params: {},
-            field: {
-              name: "",
-              get: () => undefined,
-              set: (o: any) => {},
-            },
-          }),
-        },
-        success: {
-          id: "success",
-          type: "final",
-          entry: (context, event: PlatformEvent) => {
-            if (context.data.successAction) {
-              context.data.successAction(context.data);
+        dataSchema: yup.string().required("Please enter a encryptingPin to protect your private key."),
+        navigation: {
+          next: "#unlockKey",
+        }
+      }),
+      unlockKey: {
+        id: "unlockKey",
+        invoke: {
+          src: async (context) => {
+            const km = new KeyManager(null);
+            await km.load();
+            let privateKey:string|null = null;
+            try {
+              privateKey = await km.getKey(context.data.accountAddress, context.data.decryptionPin);
+            } catch (e) {
+              context.messages["decryptionPin"] = `Couldn't decrypt your key: ${e.message}`;
+              throw e;
             }
-          },
-          data: (context, event) => {
+
+            if (!privateKey || privateKey == "") {
+              delete context.data.decryptionPin;
+              delete context.data.privateKey;
+              context.messages["decryptionPin"] = "Couldn't decrypt your key. Have you entered the correct pin?"
+              throw new Error(context.messages["decryptionPin"]);
+            }
+
+            sessionStorage.setItem("circlesKey", privateKey);
+
             delete context.data.privateKey;
-            return context.data;
+            delete context.data.decryptionPin;
           },
-        },
+          onDone: "#success",
+          onError: {
+            target: "#enterDecryptionPin"
+          }
+        }
       },
-    },
-    {
+      storeKey: {
+        id: "storeKey",
+        invoke: {
+          src: async (context) => {
+            const km = new KeyManager(null);
+            const account = RpcGateway.get().eth.accounts.privateKeyToAccount(context.data.privateKey);
+
+            await km.addEoa(account.address, account.privateKey, context.data.encryptionPin, "torus");
+
+            context.data.accountAddress = account.address;
+            sessionStorage.setItem("circlesKey", account.privateKey);
+
+            delete context.data.privateKey;
+            delete context.data.encryptionPin;
+          },
+          onDone: "#success",
+          onError: {
+            actions: (context, event) => {
+              window.o.lastError = event.data;
+            },
+            target: "#showError"
+          }
+        }
+      },
+      showError:{
+        id: "showError",
+        entry: show({ // TODO: fix <any> cast
+          component: ErrorView,
+          params: {},
+          field: {
+            name: "",
+            get:() => undefined,
+            set:(o:any) => {}
+          }
+        })
+      },
+      success: {
+        id: "success",
+        type: "final",
+        entry: (context, event: PlatformEvent) => {
+          if (context.data.successAction) {
+            context.data.successAction(context.data);
+          }
+        },
+        data: (context, event) => {
+          delete context.data.privateKey;
+          return context.data;
+        }
+      }
+    }
+  }, {
       actions: {
         assignPrivateKeyAndUserInfoToContext: (context, event) => {
           context.data.privateKey = event.data.privateKey;
           context.data.userInfo = event.data.userInfo;
-        },
-      },
-    }
-  );
+        }
+      }
+  });
 
 export const loginWithTorus: ProcessDefinition<void, LoginWithTorusContext> = {
   name: "loginWithTorus",
