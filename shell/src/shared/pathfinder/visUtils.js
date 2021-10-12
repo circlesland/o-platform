5349f1750a3184424bb1e009fa38fac2e9496475
jaensen <daniel@djanz.de>
2021-10-12 17:02:53 +0200

    completed coda task r474: "Give the “circles garden relayer” a name in TransactionGraph (old transaction)."

--- shell/src/shared/pathfinder/visUtils.js
+++ shell/src/shared/pathfinder/visUtils.js
@@ -3,6 +3,9 @@ import { AvataarGenerator } from "src/shared/avataarGenerator";
 
 export let labelFor = function (id) {
   let user = userDB[id.toLowerCase()];
+  if (id.toLowerCase() === "0x9944ce8e27ce1f16c4003f108b1c09e5ae011ba0") {
+    return "circles.garden"
+  }
   if (user && user.username) return user.username;
   else return id.substr(0, 8);
 };
@@ -11,8 +14,11 @@ export let createNodeContents = function (id) {
   let user = userDB[id.toLowerCase()];
   let node = { id: id, label: labelFor(id) };
   node["shape"] = "circularImage";
-  node["image"] =
-    user && user.avatarUrl ? user.avatarUrl : AvataarGenerator.generate(id);
+  if (id.toLowerCase() === "0x9944ce8e27ce1f16c4003f108b1c09e5ae011ba0") {
+    node["image"] = "https://dashboard.circles.garden/src/assets/logo-1ab0.svg";
+  } else {
+    node["image"] = user && user.avatarUrl ? user.avatarUrl : AvataarGenerator.generate(id);
+  }
 
   node["color"] = {
     border: "#0ad99c",
