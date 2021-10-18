92b8fa936dbff6ed6c1a8ef1b73c7b148ab9f12d
Thorsten <letsgo@codeho.net>
2021-10-18 14:20:17 +0200

    action Button SVG

--- shell/src/shared/molecules/NextNav/Components/ActionButton.svelte
+++ shell/src/shared/molecules/NextNav/Components/ActionButton.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
+import { createEventDispatcher } from "svelte";
+import Icons from "../../Icons.svelte";
 
-  export let icon: string;
+export let icon: string;
 
-  const dispatcher = createEventDispatcher();
+const dispatcher = createEventDispatcher();
 </script>
 
 <div on:click="{() => dispatcher('actionButton')}">
-  {#if icon == 'close'}
-    <img
+  {#if icon == "close"}
+    <Icons icon="close" />
+    <!-- <img
       src="/logos/close.svg"
       alt="circlesLand"
-      class="bg-white rounded-full navlogo" />
-  {:else if icon == 'logo'}
+      class="bg-white rounded-full navlogo" /> -->
+  {:else if icon == "logo"}
     <img
       src="/logos/circles.png"
       alt="circlesLand"
