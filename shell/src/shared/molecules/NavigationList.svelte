25cedbd861cfee66b0554625e4f16e0fd3d037cd
Thorsten <letsgo@codeho.net>
2021-10-18 17:26:44 +0200

    media store

--- shell/src/shared/molecules/NavigationList.svelte
+++ shell/src/shared/molecules/NavigationList.svelte
@@ -1,67 +1,79 @@
 <script lang="ts">
-  import Icons from "src/shared/molecules/Icons.svelte";
-  import { clickOutside } from "src/shared/functions/clickOutside.ts";
-  import { onMount } from "svelte";
-  import LinkPill from "src/shared/atoms/LinkPill.svelte";
-  import { getRouteList } from "src/shared/functions/getRouteList";
-  import { RuntimeDapp } from "@o-platform/o-interfaces/dist/runtimeDapp";
-  import { isMobile } from "../functions/isMobile";
+import Icons from "src/shared/molecules/Icons.svelte";
+import { clickOutside } from "src/shared/functions/clickOutside.ts";
+import { onMount } from "svelte";
+import LinkPill from "src/shared/atoms/LinkPill.svelte";
+import { getRouteList } from "src/shared/functions/getRouteList";
+import { RuntimeDapp } from "@o-platform/o-interfaces/dist/runtimeDapp";
+import { isMobile } from "../functions/isMobile";
 
-  export let runtimeDapp: RuntimeDapp<any>;
-  export let routable: RuntimeDapp<any>;
+export let runtimeDapp: RuntimeDapp<any>;
+export let routable: RuntimeDapp<any>;
 
-  let navigation = [];
+let navigation = [];
 
-  onMount(() => {
-    window.o.events.subscribe((event: any) => {
-      if (event.type !== "shell.routeChanged") return;
+onMount(() => {
+  window.o.events.subscribe((event: any) => {
+    if (event.type !== "shell.routeChanged") return;
 
-      runtimeDapp = event.runtimeDapp;
-      routable = event.routable;
-    });
+    runtimeDapp = event.runtimeDapp;
+    routable = event.routable;
   });
+});
 
-  $: {
-    if (runtimeDapp && routable) {
-      navigation = getRouteList(runtimeDapp, runtimeDapp, routable);
-    } else {
-      navigation = [];
-    }
+$: {
+  if (runtimeDapp && routable) {
+    navigation = getRouteList(runtimeDapp, runtimeDapp, routable);
+  } else {
+    navigation = [];
   }
-  //  use:clickOutside
-  // on:click_outside="{e => {
-  //   if (isMobile()) {
-  //     window.o.publishEvent({ type: 'shell.closeNavigation' });
-  //     e.stopImmediatePropagation();
-  //   }
-  // }}"
+}
+//  use:clickOutside
+// on:click_outside="{e => {
+//   if (isMobile()) {
+//     window.o.publishEvent({ type: 'shell.closeNavigation' });
+//     e.stopImmediatePropagation();
+//   }
+// }}"
 </script>
 
 <div class="z-10 flex flex-col flex-1">
   <nav class="flex flex-col flex-1 p-4 mt-4 w-52"></nav>
   <div class="relative flex-shrink-0 pt-4 pb-12 space-y-2 w-52">
     <LinkPill
-      props="{{ text: 'Legal Notice', link: '/#/homepage/tos', extern: true, isSmall: true }}" />
+      props="{{
+        text: 'Legal Notice',
+        link: '/#/homepage/tos',
+        extern: true,
+        isSmall: true,
+      }}" />
     <LinkPill
-      props="{{ text: 'Terms & Privacy', link: '/#/homepage/tos', extern: true, isSmall: true }}" />
+      props="{{
+        text: 'Terms & Privacy',
+        link: '/#/homepage/tos',
+        extern: true,
+        isSmall: true,
+      }}" />
     <LinkPill
-      props="{{ text: 'Help & Support', link: '/#/homepage/tos', extern: true, isSmall: true }}" />
+      props="{{
+        text: 'Help & Support',
+        link: '/#/homepage/tos',
+        extern: true,
+        isSmall: true,
+      }}" />
     {#if navigation}
       {#each navigation as navItem}
         <LinkPill
-          props="{{ icon: navItem.icon, text: navItem.title, link: navItem.url, isActive: navItem.isActive }}" />
+          props="{{
+            icon: navItem.icon,
+            text: navItem.title,
+            link: navItem.url,
+            isActive: navItem.isActive,
+          }}" />
       {/each}
     {/if}
   </div>
 </div>
-{#if isMobile()}
-  <div
-    class="fixed z-50 flex justify-center flex-shrink-0 w-12 h-12 px-3 py-4 ml-4 bg-white rounded-full cursor-pointer bottom-6 left-72"
-    on:click="{() => window.o.publishEvent({ type: 'shell.closeNavigation' })}">
-    <Icons icon="buttonleftarrow" />
-  </div>
-{/if}
 
 <style>
-
 </style>
