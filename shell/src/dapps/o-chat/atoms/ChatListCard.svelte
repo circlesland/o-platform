fc8022d77bf5eab79ca97c39f347233a666c85d4
Thorsten <letsgo@codeho.net>
2021-10-18 14:10:21 +0200

    param change, chatlist prepare for lazy list

--- shell/src/dapps/o-chat/atoms/ChatListCard.svelte
+++ shell/src/dapps/o-chat/atoms/ChatListCard.svelte
@@ -1,64 +1,71 @@
 <script lang="ts">
-  import { transfer } from "../../o-banking/processes/transfer";
+import { transfer } from "../../o-banking/processes/transfer";
 
-  import { push } from "svelte-spa-router";
+import { push } from "svelte-spa-router";
 
-  import ItemCard from "../../../shared/atoms/ItemCard.svelte";
-  import { Contact } from "../../../shared/api/data/types";
-  import { onMount } from "svelte";
-  import Date from "../../../shared/atoms/Date.svelte";
-  import { Profile } from "../../../dapps/o-banking/data/api/types";
-  export let contact: Contact;
+import ItemCard from "../../../shared/atoms/ItemCard.svelte";
+import { Contact } from "../../../shared/api/data/types";
+import { onMount } from "svelte";
+import Date from "../../../shared/atoms/Date.svelte";
+import { Profile } from "../../../dapps/o-banking/data/api/types";
 
-  let pictureUrl: string;
-  let displayName: string;
-  let safeAddress: string;
-  let message: string;
-  let contactProfile: Profile;
+export let param: Contact;
 
-  onMount(() => {
-    contactProfile = contact.contactAddressProfile;
-    displayName =
-      contactProfile.firstName +
-      (contactProfile.lastName ? " " + contactProfile.lastName : "");
-    safeAddress = contactProfile.circlesAddress;
-    message = "";
-    if (contact.trustsYou > 0 && contact.youTrust > 0) {
-      message += "mutual trust | ";
-    } else if (!contact.trustsYou && contact.youTrust > 0) {
-      message += "trusted by you | ";
-    } else if (contact.trustsYou > 0 && !contact.youTrust) {
-      message += "is trusting you | ";
-    } else {
-      message += "not trusted | ";
-    }
-    message += contact.lastEvent ? contact.lastEvent.payload.__typename : "";
+console.log("PARASM: ", param);
+let pictureUrl: string;
+let displayName: string;
+let safeAddress: string;
+let message: string;
+let contactProfile: Profile;
 
-    // console.log("CONTACT: ", contact);
-  });
-
-  function loadDetailPage(path) {
-    push(`#/chat/${path}`);
+onMount(() => {
+  contactProfile = param.contactAddressProfile;
+  displayName =
+    contactProfile.firstName +
+    (contactProfile.lastName ? " " + contactProfile.lastName : "");
+  safeAddress = contactProfile.circlesAddress;
+  message = "";
+  if (param.trustsYou > 0 && param.youTrust > 0) {
+    message += "mutual trust | ";
+  } else if (!param.trustsYou && param.youTrust > 0) {
+    message += "trusted by you | ";
+  } else if (param.trustsYou > 0 && !param.youTrust) {
+    message += "is trusting you | ";
+  } else {
+    message += "not trusted | ";
   }
+  message += param.lastEvent ? param.lastEvent.payload.__typename : "";
 
-  function goToProfile(e, path?: string) {
-    if (!path) return;
-    e.stopPropagation();
-    push(`#/friends/${path}`);
-  }
+  // console.log("CONTACT: ", contact);
+});
+
+function loadDetailPage(path) {
+  push(`#/chat/${path}`);
+}
+
+function goToProfile(e, path?: string) {
+  if (!path) return;
+  e.stopPropagation();
+  push(`#/friends/${path}`);
+}
 </script>
 
 <div on:click="{() => loadDetailPage(safeAddress)}">
   <ItemCard
-    params="{{ edgeless: false, imageProfile: contactProfile, title: displayName, subTitle: message, truncateMain: true }}">
-
+    params="{{
+      edgeless: false,
+      imageProfile: contactProfile,
+      title: displayName,
+      subTitle: message,
+      truncateMain: true,
+    }}">
     <div slot="itemCardEnd">
       <div class="self-end text-right">
         <span>&nbsp;</span>
       </div>
       <div class="self-end text-xs text-dark-lightest whitespace-nowrap">
-        {#if contact.lastContactAt}
-          <Date time="{contact.lastContactAt}" />
+        {#if param.lastContactAt}
+          <Date time="{param.lastContactAt}" />
         {/if}
       </div>
     </div>
--- shell/src/dapps/o-chat/pages/Chat2.svelte
+++ shell/src/dapps/o-chat/pages/Chat2.svelte
@@ -8,51 +8,46 @@ import { Contact, ContactsDocument } from "../../../shared/api/data/types";
 import ChatListCard from "../atoms/ChatListCard.svelte";
 import { PlatformEvent } from "@o-platform/o-events/dist/platformEvent";
 import { Subscription } from "rxjs";
+import List from "../../../shared/molecules/Lists/List.svelte";
 
 export let runtimeDapp: RuntimeDapp<any>;
 export let routable: Routable;
 
 let error: string | undefined = undefined;
 let contacts: Contact[] = [];
-let shellEventSubscription: Subscription;
+// let shellEventSubscription: Subscription;
 
-async function reload() {
-  const safeAddress = $me.circlesAddress;
-  const apiClient = await window.o.apiClient.client.subscribeToResult();
-  const contactsResult = await apiClient.query({
-    query: ContactsDocument,
-    variables: {
-      safeAddress,
-    },
-  });
-  if (contactsResult.errors?.length > 0) {
-    error = `Couldn't read the contacts of safe ${safeAddress}: \n${contactsResult.errors
-      .map((o) => o.message)
-      .join("\n")}`;
-    return;
-  }
-  contacts = contactsResult.data.contacts;
-}
+// onMount(async () => {
+//   shellEventSubscription = window.o.events.subscribe(
+//       async (event: PlatformEvent) => {
+//         if (event.type != "shell.refresh" || (<any>event).dapp != "chat:1") {
+//           return;
+//         }
+//         await reload();
+//       }
+//   );
+//   await reload();
+// });
 
-onMount(async () => {
-  shellEventSubscription = window.o.events.subscribe(
-    async (event: PlatformEvent) => {
-      if (event.type != "shell.refresh" || (<any>event).dapp != "chat:1") {
-        return;
-      }
-      await reload();
-    }
-  );
-  await reload();
-});
+// onDestroy(() => shellEventSubscription.unsubscribe());
 
-onDestroy(() => shellEventSubscription.unsubscribe());
+const listArguments = {
+  safeAddress: $me.circlesAddress,
+  limit: 20,
+};
 </script>
 
 <SimpleHeader runtimeDapp="{runtimeDapp}" routable="{routable}" />
 
 <div class="px-4 mx-auto -mt-3 md:w-2/3 xl:w-1/2">
-  {#if !contacts && !error}
+  <List
+    listItemType="{Contact}"
+    listItemComponent="{ChatListCard}"
+    fetchQuery="{ContactsDocument}"
+    fetchQueryArguments="{listArguments}"
+    dataKey="contacts" />
+
+  <!-- {#if !contacts && !error}
     <section class="flex items-center justify-center mb-2 ">
       <div class="flex items-center w-full p-4 space-x-2 bg-white shadow ">
         <div class="flex flex-col items-start">
@@ -73,9 +68,9 @@ onDestroy(() => shellEventSubscription.unsubscribe());
       </div>
     </section>
   {:else}
-    <!-- TODO: Possible actions: trust, transfer money -->
-    {#each contacts.filter((o) => !!o.contactAddressProfile) as contact}
-      <ChatListCard param="{contact}" />
+    
+    {#each contacts.filter(o => !!o.contactAddressProfile) as contact}
+      <ChatListCard {contact} />
     {/each}
-  {/if}
+  {/if} -->
 </div>
