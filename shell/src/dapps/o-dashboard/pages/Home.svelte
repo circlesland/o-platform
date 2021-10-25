24ef3afe14131de11cb4a8d36b48d811d99fee1d
Thorsten <letsgo@codeho.net>
2021-10-25 11:30:17 +0200

    Auto stash before rebase of "origin/dev"

--- shell/src/dapps/o-dashboard/pages/Home.svelte
+++ shell/src/dapps/o-dashboard/pages/Home.svelte
@@ -100,7 +100,7 @@ div.mx-auto(class="md:w-2/3 xl:w-1/2")
             Icons(icon="dashpassport")
           div.mt-4.text-3xl.font-heading.text-dark passport
 
-      section.flex.items-center.justify-center.bg-white.rounded-lg.shadow-md.cursor-pointer.dashboard-card(on:click!="{() => loadLink('/friends')}")
+      section.flex.items-center.justify-center.bg-white.rounded-lg.shadow-md.cursor-pointer.dashboard-card(on:click!="{() => loadLink('/friends/chat')}")
         div.flex.flex-col.items-center.w-full.p-4.pt-6.justify-items-center
           div.pt-2.text-primary
             Icons(icon="dashfriends")
