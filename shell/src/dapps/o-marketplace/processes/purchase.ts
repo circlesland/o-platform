7bb336335ef3f37b6caad39842f39b68fec870b6
Thorsten <letsgo@codeho.net>
2021-11-04 19:32:56 +0100

    chat notification, checkout

--- shell/src/dapps/o-marketplace/processes/purchase.ts
+++ shell/src/dapps/o-marketplace/processes/purchase.ts
@@ -2,41 +2,61 @@ import { ProcessDefinition } from "@o-platform/o-process/dist/interfaces/process
 import { ProcessContext } from "@o-platform/o-process/dist/interfaces/processContext";
 import { fatalError } from "@o-platform/o-process/dist/states/fatalError";
 import { createMachine } from "xstate";
+import { prompt } from "@o-platform/o-process/dist/states/prompt";
 import { PlatformEvent } from "@o-platform/o-events/dist/platformEvent";
+import { EditorViewContext } from "@o-platform/o-editors/src/shared/editorViewContext";
+import TransferSummary from "../../o-banking/atoms/TransferSummary.svelte";
 
-export type PurchaseContextData = {
-};
+export type PurchaseContextData = {};
 
 export type PurchaseContext = ProcessContext<PurchaseContextData>;
 
-const strings = {
+const editorContent: { [x: string]: EditorViewContext } = {
+  summary: {
+    title: "Check out",
+    description: "thank you, we transferred all your money to us. Goodbye",
+    placeholder: "",
+    submitButtonText: "Pay now",
+  },
 };
 
 const processDefinition = (processId: string) =>
-    createMachine<PurchaseContext, any>({
-        id: `${processId}:createOffer`,
-        initial: "title",
-        states: {
-            // Include a default 'error' state that propagates the error by re-throwing it in an action.
-            // TODO: Check if this works as intended
-            ...fatalError<PurchaseContext, any>("error"),
+  createMachine<PurchaseContext, any>({
+    id: `${processId}:createOffer`,
+    initial: "acceptSummary",
+    states: {
+      // Include a default 'error' state that propagates the error by re-throwing it in an action.
+      // TODO: Check if this works as intended
+      ...fatalError<PurchaseContext, any>("error"),
 
-            success: {
-                type: "final",
-                id: "success",
-                data: (context, event: any) => {
-                    // console.log(`enter: upsertIdentity.success`, context.data);
-                    window.o.publishEvent(<PlatformEvent>{
-                        type: "shell.authenticated",
-                        profile: context.data,
-                    });
-                    return event.data;
-                },
-            },
+      acceptSummary: prompt<PurchaseContext, any>({
+        field: "acceptSummary",
+        component: TransferSummary,
+        params: {
+          view: editorContent.summary,
+          submitButtonText: editorContent.summary.submitButtonText,
+          html: () => "",
         },
-    });
+        navigation: {
+          next: "#success",
+        },
+      }),
+      success: {
+        type: "final",
+        id: "success",
+        data: (context, event: any) => {
+          // console.log(`enter: upsertIdentity.success`, context.data);
+          window.o.publishEvent(<PlatformEvent>{
+            type: "shell.authenticated",
+            profile: context.data,
+          });
+          return event.data;
+        },
+      },
+    },
+  });
 
 export const purchase: ProcessDefinition<void, PurchaseContextData> = {
-    name: "purchase",
-    stateMachine: <any>processDefinition,
+  name: "purchase",
+  stateMachine: <any>processDefinition,
 };
--- shell/src/shared/NotificationViewer.svelte
+++ shell/src/shared/NotificationViewer.svelte
@@ -1,255 +1,255 @@
 <script lang="ts">
-  import {Continue} from "@o-platform/o-process/dist/events/continue";
+import { Continue } from "@o-platform/o-process/dist/events/continue";
 
-  import NotificationViewChatMessage from "./NotificationViewer/atoms/NotificationViewChatMessage.svelte";
-  import NotificationViewUbi from "./NotificationViewer/NotificationViewUbi.svelte";
-  import NotificationViewTrust from "./NotificationViewer/atoms/NotificationViewTrust.svelte";
-  import GenericEventCard from "./GenericEventCard.svelte";
-  import NotificationViewTransfer from "./NotificationViewer/atoms/NotificationViewTransfer.svelte";
+import NotificationViewChatMessage from "./NotificationViewer/atoms/NotificationViewChatMessage.svelte";
+import NotificationViewUbi from "./NotificationViewer/NotificationViewUbi.svelte";
+import NotificationViewTrust from "./NotificationViewer/atoms/NotificationViewTrust.svelte";
+import GenericEventCard from "./GenericEventCard.svelte";
+import NotificationViewTransfer from "./NotificationViewer/atoms/NotificationViewTransfer.svelte";
 
-  import {inbox} from "./stores/inbox";
-  import {NotificationViewerContext} from "@o-platform/o-editors/src/notificationViewerContext";
-  import ProcessNavigation from "../../../packages/o-editors/src/ProcessNavigation.svelte";
-  import {me} from "./stores/me";
-  import {setTrust} from "../dapps/o-banking/processes/setTrust";
+import { inbox } from "./stores/inbox";
+import { NotificationViewerContext } from "@o-platform/o-editors/src/notificationViewerContext";
+import ProcessNavigation from "../../../packages/o-editors/src/ProcessNavigation.svelte";
+import { me } from "./stores/me";
+import { setTrust } from "../dapps/o-banking/processes/setTrust";
 
-  export let context: NotificationViewerContext;
+export let context: NotificationViewerContext;
 
-  let data: any = context.data[context.field];
-  let eventData: any = null;
+let data: any = context.data[context.field];
+let eventData: any = null;
 
-  const components = [
-    {type: "ChatMessage", component: NotificationViewChatMessage},
-    {type: "CrcMinting", component: NotificationViewUbi},
-    {type: "CrcTrust", component: NotificationViewTrust},
-    {type: "CrcHubTransfer", component: NotificationViewTransfer},
-  ];
+const components = [
+  { type: "chat_message", component: NotificationViewChatMessage },
+  { type: "CrcMinting", component: NotificationViewUbi },
+  { type: "CrcTrust", component: NotificationViewTrust },
+  { type: "CrcHubTransfer", component: NotificationViewTransfer },
+];
 
-  function submit() {
-    const answer = new Continue();
-    answer.data = context.data;
-    context.process.sendAnswer(answer);
-  }
+function submit() {
+  const answer = new Continue();
+  answer.data = context.data;
+  context.process.sendAnswer(answer);
+}
 
-  function buildDataModel(data) {
-    console.log("DATA: ", data);
-    let notificationType: string = null;
-    let title: string = null;
+function buildDataModel(data) {
+  console.log("DATA: ", data);
+  let notificationType: string = null;
+  let title: string = null;
 
-    let type: string = data.type;
-    let icon: string = null;
-    let limit: number = null;
-    let value: string = null;
+  let type: string = data.type;
+  let icon: string = null;
+  let limit: number = null;
+  let value: string = null;
 
-    let targetCirclesAddress: string = data.payload.from;
+  let targetCirclesAddress: string = data.payload.from;
 
-    let profile: any;
-    let actions: {
-      title: string;
-      icon: string;
-      colorClass: string;
-      action: () => void;
-    }[] = [];
+  let profile: any;
+  let actions: {
+    title: string;
+    icon: string;
+    colorClass: string;
+    action: () => void;
+  }[] = [];
 
-    switch (data.type) {
-      case "ChatMessage":
-        notificationType = "chat_message";
-        title = `${data.payload.text}`;
+  switch (data.type) {
+    case "ChatMessage":
+      notificationType = "chat_message";
+      title = `${data.payload.text}`;
 
-        profile = data.payload.from_profile;
-        actions = [
-          {
-            title: "Go to Chat",
-            icon: "chat",
-            colorClass: "",
-            action: () => {
-              context.params.push(`#/friends/chat/${data.payload.from}`);
-              inbox.acknowledge(data);
-            },
+      profile = data.payload.from_profile;
+      actions = [
+        {
+          title: "Go to Chat",
+          icon: "chat",
+          colorClass: "",
+          action: () => {
+            context.params.push(`#/friends/chat/${data.payload.from}`);
+            inbox.acknowledge(data);
           },
-        ];
-        break;
-      case "CrcMinting":
-        notificationType = "crc_minting";
-        value = data.value;
+        },
+      ];
+      break;
+    case "CrcMinting":
+      notificationType = "crc_minting";
+      value = data.value;
+      actions = [
+        {
+          title: "Show details",
+          icon: "",
+          colorClass: "",
+          action: () => {
+            context.params.push(
+              `#/banking/transactions/${data.transaction_hash}`
+            );
+            inbox.acknowledge(data);
+          },
+        },
+      ];
+      break;
+    case "CrcTrust":
+      profile = data.payload.can_send_to_profile;
+      targetCirclesAddress = data.payload.can_send_to;
+      limit = data.payload.limit;
+
+      if (data.payload.limit == 0) {
+        notificationType = "trust_removed";
+        icon = "untrust";
         actions = [
           {
-            title: "Show details",
+            title: "Show Profile",
             icon: "",
             colorClass: "",
             action: () => {
               context.params.push(
-                      `#/banking/transactions/${data.transaction_hash}`
-              );
-              inbox.acknowledge(data);
-            },
-          },
-        ];
-        break;
-      case "CrcTrust":
-        profile = data.payload.can_send_to_profile;
-        targetCirclesAddress = data.payload.can_send_to;
-        limit = data.payload.limit;
-
-        if (data.payload.limit == 0) {
-          notificationType = "trust_removed";
-          icon = "untrust";
-          actions = [
-            {
-              title: "Show Profile",
-              icon: "",
-              colorClass: "",
-              action: () => {
-                context.params.push(
-                        `#/friends/${
-                                data.payload.can_send_to_profile
-                                        ? data.payload.can_send_to_profile.circlesAddress
-                                        : data.payload.from
-                        }`
-                );
-                // submit(); TODO: ADD BACK IN TO MARK EVENT AS READ
-              },
-            },
-            {
-              title: `Untrust ${
-                      data.payload.can_send_to_profile
-                              ? data.payload.can_send_to_profile.firstName
-                              : data.payload.can_send_to
-              }`,
-              icon: "untrust",
-              colorClass: "",
-              action: () => {
-                window.o.runProcess(setTrust, {
-                  trustLimit: 0,
-                  trustReceiver: data.payload.can_send_to,
-                  safeAddress: $me.circlesAddress,
-                  privateKey: localStorage.getItem("circlesKey"),
-                });
-              },
-            },
-          ];
-        } else if (data.payload.limit > 0) {
-          notificationType = "trust_added";
-
-          icon = "trust";
-          actions = [
-            {
-              title: "Show Profile",
-              icon: "",
-              colorClass: "",
-              action: () => {
-                context.params.push(
-                        `#/friends/${
-                                data.payload.can_send_to_profile
-                                        ? data.payload.can_send_to_profile.circlesAddress
-                                        : data.payload.can_send_to
-                        }`
-                );
-                // submit(); TODO: ADD BACK IN TO MARK EVENT AS READ
-              },
-            },
-            {
-              title: `Trust ${
-                      data.payload.can_send_to_profile
-                              ? data.payload.can_send_to_profile.firstName
-                              : data.payload.can_send_to
-              }`,
-              icon: "trust",
-              colorClass: "",
-              action: () => {
-                window.o.runProcess(setTrust, {
-                  trustLimit: 100,
-                  trustReceiver: data.payload.can_send_to,
-                  safeAddress: $me.circlesAddress,
-                  privateKey: localStorage.getItem("circlesKey"),
-                });
-              },
-            },
-          ];
-        }
-        break;
-      case "CrcHubTransfer":
-        profile = data.payload.from_profile;
-        value = data.value;
-        notificationType = "transfer_in";
-        icon = "sendmoney";
-
-        actions = [
-          {
-            title: "Show details",
-            icon: "",
-            colorClass: "",
-            action: () => {
-              context.params.push(
-                      `#/banking/transactions/${data.transaction_hash}`
+                `#/friends/${
+                  data.payload.can_send_to_profile
+                    ? data.payload.can_send_to_profile.circlesAddress
+                    : data.payload.from
+                }`
               );
               // submit(); TODO: ADD BACK IN TO MARK EVENT AS READ
             },
           },
-          // {
-          //   title: `Send Circles to ${
-          //     data.payload.from_profile
-          //       ? data.payload.from_profile.firstName
-          //       : data.payload.from
-          //   }`,
-          //   icon: "sendmoney",
-          //   colorClass: "",
-          //   action: () => {
-          //     window.o.runProcess(transfer, {
-          //       safeAddress: $me.circlesAddress,
-          //       recipientAddress: data.payload.from,
-          //       privateKey: localStorage.getItem("circlesKey"),
-          //     });
-          //   },
-          // },
+          {
+            title: `Untrust ${
+              data.payload.can_send_to_profile
+                ? data.payload.can_send_to_profile.firstName
+                : data.payload.can_send_to
+            }`,
+            icon: "untrust",
+            colorClass: "",
+            action: () => {
+              window.o.runProcess(setTrust, {
+                trustLimit: 0,
+                trustReceiver: data.payload.can_send_to,
+                safeAddress: $me.circlesAddress,
+                privateKey: localStorage.getItem("circlesKey"),
+              });
+            },
+          },
         ];
-        break;
-    }
+      } else if (data.payload.limit > 0) {
+        notificationType = "trust_added";
 
-    let text = data.tags?.find(
-            (o) => o.typeId === "o-banking:transfer:message:1"
-    )?.value;
-    if (!text) {
-      text = "";
-    }
+        icon = "trust";
+        actions = [
+          {
+            title: "Show Profile",
+            icon: "",
+            colorClass: "",
+            action: () => {
+              context.params.push(
+                `#/friends/${
+                  data.payload.can_send_to_profile
+                    ? data.payload.can_send_to_profile.circlesAddress
+                    : data.payload.can_send_to
+                }`
+              );
+              // submit(); TODO: ADD BACK IN TO MARK EVENT AS READ
+            },
+          },
+          {
+            title: `Trust ${
+              data.payload.can_send_to_profile
+                ? data.payload.can_send_to_profile.firstName
+                : data.payload.can_send_to
+            }`,
+            icon: "trust",
+            colorClass: "",
+            action: () => {
+              window.o.runProcess(setTrust, {
+                trustLimit: 100,
+                trustReceiver: data.payload.can_send_to,
+                safeAddress: $me.circlesAddress,
+                privateKey: localStorage.getItem("circlesKey"),
+              });
+            },
+          },
+        ];
+      }
+      break;
+    case "CrcHubTransfer":
+      profile = data.payload.from_profile;
+      value = data.value;
+      notificationType = "transfer_in";
+      icon = "sendmoney";
 
-    return {
-      safeAddress: data.safe_address,
-      targetCirclesAddress: targetCirclesAddress,
-      type: type,
-      profile: profile ? profile : null,
-      time: data.timestamp,
-      fullWidth: true,
-      value: value,
-      limit: limit,
-      notificationType: notificationType,
-      title: title,
-      icon: icon,
-      actions: actions,
-      text: text,
-    };
+      actions = [
+        {
+          title: "Show details",
+          icon: "",
+          colorClass: "",
+          action: () => {
+            context.params.push(
+              `#/banking/transactions/${data.transaction_hash}`
+            );
+            // submit(); TODO: ADD BACK IN TO MARK EVENT AS READ
+          },
+        },
+        // {
+        //   title: `Send Circles to ${
+        //     data.payload.from_profile
+        //       ? data.payload.from_profile.firstName
+        //       : data.payload.from
+        //   }`,
+        //   icon: "sendmoney",
+        //   colorClass: "",
+        //   action: () => {
+        //     window.o.runProcess(transfer, {
+        //       safeAddress: $me.circlesAddress,
+        //       recipientAddress: data.payload.from,
+        //       privateKey: localStorage.getItem("circlesKey"),
+        //     });
+        //   },
+        // },
+      ];
+      break;
   }
 
-  eventData = buildDataModel(data);
-  console.log("eventData: ", eventData);
-
-  function handleClick(action) {
-    if (action.event) {
-      window.o.publishEvent(action.event);
-    }
-    if (action.action) {
-      action.action();
-    }
+  let text = data.tags?.find(
+    (o) => o.typeId === "o-banking:transfer:message:1"
+  )?.value;
+  if (!text) {
+    text = "";
   }
 
-  function getEventView() {
-    const specificView = components.find((x) => x.type === eventData.type);
+  return {
+    safeAddress: data.safe_address,
+    targetCirclesAddress: targetCirclesAddress,
+    type: type,
+    profile: profile ? profile : null,
+    time: data.timestamp,
+    fullWidth: true,
+    value: value,
+    limit: limit,
+    notificationType: notificationType,
+    title: title,
+    icon: icon,
+    actions: actions,
+    text: text,
+  };
+}
 
-    if (!specificView)
-      return {component: GenericEventCard};
+eventData = buildDataModel(data);
+console.log("eventData: ", eventData);
 
-    return specificView;
+function handleClick(action) {
+  if (action.event) {
+    window.o.publishEvent(action.event);
   }
+  if (action.action) {
+    action.action();
+  }
+}
+
+function getEventView() {
+  console.log("VIEW: ", eventData.type);
+  const specificView = components.find((x) => x.type === eventData.type);
+
+  if (!specificView) return { component: GenericEventCard };
+
+  return specificView;
+}
 </script>
 
 <div>
