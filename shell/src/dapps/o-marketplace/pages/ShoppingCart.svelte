3a7946ebb20d725b28b1e4d3399dc618b1e42693
Thorsten Schau <letsgo@codeho.net>
2021-11-24 10:34:05 +0100

    Remove Close Cart button #536

--- shell/src/dapps/o-marketplace/pages/ShoppingCart.svelte
+++ shell/src/dapps/o-marketplace/pages/ShoppingCart.svelte
@@ -23,27 +23,26 @@ div.p-5
       h1.text-3xl.uppercase.font-heading Cart
   +if('$cartContents && $cartContents.length > 0')  
     div
-      .w-full
+      div.w-full
         div(class="md:flex")
-          .w-full
+          div.w-full
             div.pt-6(class="md:grid")
                 
                 CartItems(cartContents="{cartContents}")
                 
-                .flex.justify-end.items-center
+                div.flex.justify-end.items-center
                   span.text-sm.font-medium.text-gray-400.mr-2 Total:
                   span.text-lg.font-bold {$totalPrice.toFixed(2)} ⦿
                   
-                .flex.justify-center.items-center.mt-6
+                div.flex.justify-center.items-center.mt-6
                 
-                  .flex.flex-row.w-full.space-x-4
-                    div
-                      button.h-auto.btn-block.btn.btn-light(on:click!="{() =>  history.back()}") Close Cart
-                    .flex-grow
+                  div.flex.flex-row.w-full.space-x-4
+
+                    div.flex-grow
                       button.h-auto.btn-block.btn.btn-primary(on:click!="{() =>  checkout()}") Check Out
     +else
       p.text-center.mt-6 Your cart is empty!
-      .w-full.mt-6
+      div.w-full.mt-6
           div
           button.h-auto.btn-block.btn.btn-light(on:click!="{(event) =>  handleClickOutside(event)}") Continue Shopping
 </template>
