3a22e197d470fa6ce5cae50546378f35ade62ef0
Thorsten <letsgo@codeho.net>
2021-12-07 15:20:08 +0100

    fix orga name on dashboard
    #578

--- shell/src/dapps/o-dashboard/atoms/DashboardHeader.svelte
+++ shell/src/dapps/o-dashboard/atoms/DashboardHeader.svelte
@@ -1,23 +1,33 @@
 <script lang="ts">
-  import { me } from "../../../shared/stores/me";
-  import TopNav from "src/shared/atoms/TopNav.svelte";
-  import PageHeader from "src/shared/atoms/PageHeader.svelte";
-  import { RuntimeDapp } from "@o-platform/o-interfaces/dist/runtimeDapp";
-  import { Routable } from "@o-platform/o-interfaces/dist/routable";
-  $: me;
+import { me } from "../../../shared/stores/me";
+import TopNav from "src/shared/atoms/TopNav.svelte";
+import PageHeader from "src/shared/atoms/PageHeader.svelte";
+import { RuntimeDapp } from "@o-platform/o-interfaces/dist/runtimeDapp";
+import { Routable } from "@o-platform/o-interfaces/dist/routable";
+$: me;
 
-  export let runtimeDapp: RuntimeDapp<any>;
-  export let routable: Routable;
+export let runtimeDapp: RuntimeDapp<any>;
+export let routable: Routable;
+let name: string = "";
+
+$: {
+  if ($me) {
+    if ($me.__typename == "Profile") {
+      name = $me.firstName ? $me.firstName : "";
+    } else if ($me.__typename == "Organisation") {
+      name = $me.name ? $me.name : "";
+    }
+  }
+}
 </script>
 
-<TopNav {runtimeDapp} {routable} />
+<TopNav runtimeDapp="{runtimeDapp}" routable="{routable}" />
 
 <PageHeader heightClass="h-28">
-
   <div class="self-center flex-grow text-center justify-self-start">
     <div class="text-xl">
       <h1 class="text-4xl text-white font-heading">
-        Welcome {$me ? $me.firstName : ''}
+        Welcome {name}
       </h1>
     </div>
   </div>
