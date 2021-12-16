dcd7278fd0bb0721696e96cd3a9e24cd3a648063
Thorsten <letsgo@codeho.net>
2021-12-16 22:31:52 +0100

    Runde ecken für den sam

--- shell/src/shared/layouts/Layout.svelte
+++ shell/src/shared/layouts/Layout.svelte
@@ -152,7 +152,9 @@ function onkeydown(e: KeyboardEvent) {
           class="fixed left-0 z-10 flex flex-col items-center justify-end w-full h-12 bottom-20"
           class:hidden="{menuOpen}">
           <section class="mb-4">
-            <button class="btn btn-primary" on:click="{action.action}">
+            <button
+              class="rounded-full btn btn-primary"
+              on:click="{action.action}">
               {action.text}
             </button>
           </section>
