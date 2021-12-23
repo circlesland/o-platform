326e74417c781792a98d4d07566d0ff4a473a508
jaensen <daniel@djanz.de>
2021-12-23 16:25:50 +0100

    fixed double currency symbols in chat overview

--- shell/src/dapps/o-contacts/atoms/ChatListCard.svelte
+++ shell/src/dapps/o-contacts/atoms/ChatListCard.svelte
@@ -90,7 +90,7 @@ if (mostRecentDisplayEvent.direction == ContactDirection.In) {
         mostRecentDisplayEvent.value,
         null,
         "EURS"
-      )} ${Currency.currencySymbol[$me.displayCurrency]} €`;
+      )} ${Currency.currencySymbol[$me.displayCurrency]}`;
       break;
     case EventType.Erc20Transfer:
       message = `${displayName} sent you ${displayCirclesAmount(
