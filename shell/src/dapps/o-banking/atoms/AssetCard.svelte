3d8bc3daa0997fe039783e37258864805c23fbea
Thorsten Schau <letsgo@codeho.net>
2022-01-18 15:36:57 +0100

    #621 - add euro amount to crc assetcard

--- shell/src/dapps/o-banking/atoms/AssetCard.svelte
+++ shell/src/dapps/o-banking/atoms/AssetCard.svelte
@@ -3,6 +3,7 @@ import { push } from "svelte-spa-router";
 import Card from "src/shared/atoms/Card.svelte";
 import ItemCard from "../../../shared/atoms/ItemCard.svelte";
 import { AssetBalance } from "../../../shared/api/data/types";
+import { Currency } from "../../../shared/currency";
 
 export let symbol: string;
 export let title: string;
@@ -11,9 +12,16 @@ export let variety: number;
 export let description: string;
 
 let pictureUrl: string;
+let varietyDetail;
 
 $: {
   pictureUrl = symbol;
+  varietyDetail =
+    variety == 0 || variety == 1
+      ? title
+      : variety
+      ? variety + " different " + title
+      : "";
 }
 </script>
 
@@ -25,15 +33,13 @@ $: {
       edgeless: false,
       imageUrl: `/logos/${symbol}.png`,
       title: title,
-      subTitle: description,
+      subTitle: varietyDetail || description,
       truncateMain: true,
       endTextBig: Number.parseFloat(balance).toFixed(2),
       endTextSmall:
         variety == 0 || variety == 1
           ? title
-          : variety
-          ? variety + ' different ' + title
-          : '',
+          : `${Number.parseFloat(balance / 10).toFixed(2)} €`,
       mobileTextCutoff: 18,
     }}" />
 </div>
