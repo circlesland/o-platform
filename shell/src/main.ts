8b886ffe0797f9a273d9df7d9d9b4a8092c858a3
jaensen <daniel@circles.name>
2022-02-10 15:56:47 +0100

    wrote a little helper to use the svelte-i18n library in plain *.ts files

--- shell/src/main.ts
+++ shell/src/main.ts
@@ -2,29 +2,16 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import {IShell} from "./shell";
 import {ProcessContext} from "@o-platform/o-process/dist/interfaces/processContext";
-import App from "src/App.svelte";
 import {RpcGateway} from "@o-platform/o-circles/dist/rpcGateway";
 dayjs.extend(relativeTime)
 RpcGateway.setup("__RPC_ENDPOINT__");
-
-import { addMessages, init } from "svelte-i18n";
-
-const en = require("./i18n/lang/en.json") ;
-
-addMessages("en", en);
-
-init({
-    fallbackLocale: "en",
-    initialLocale: "en",
-});
-
-
 
 // TODO: Use a service like 'https://github.com/ipfs/js-ipfs/blob/6870873f0696bb5d8d91fce4a4ef1f7420443993/packages/ipfs-message-port-server/src/server.js#L134'
 //       to share data between different app domains.
 declare global {
   interface Window {
-    o: IShell
+    o: IShell,
+    i18n: (id: string, options?: any) => string
   }
 }
 
@@ -41,6 +28,7 @@ export async function getProcessContext(): Promise<ProcessContext<any>> {
 
 (<any>window).rpcGateway = RpcGateway.get();
 
+import App from "src/App.svelte";
 export default new App({
   target: document.body,
 });
