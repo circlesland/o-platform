b4415abbff38d0603cdfc78af2a43e92d213e661
jaensen <daniel@circles.name>
2022-03-09 23:17:14 +0100

    fixed i18n key

--- shell/src/shared/NotificationViewer/molecules/NotificationViewWelcome.svelte
+++ shell/src/shared/NotificationViewer/molecules/NotificationViewWelcome.svelte
@@ -7,6 +7,6 @@
     class="flex flex-col items-center self-center w-full m-auto text-center justify-self-center ">
     <div class="mt-4 text-3xl font-heading">{$_("shared.molecules.notificationViewer.molecules.notificationViewWelcome.welcomeToCirclesLand")}</div>
 
-    <div class="mt-1 text-sm text-dark-lightest">{$_("shared.molecules.notificationViewer.molecules.notificationViewWelcome.doSchoenerMensch")}</div>
+    <div class="mt-1 text-sm text-dark-lightest">{$_("shared.molecules.notificationViewer.molecules.notificationViewWelcome.duSchoenerMensch")}</div>
   </div>
 </div>
