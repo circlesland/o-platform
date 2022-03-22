a76b67ee15feaa5b1d562a5cd90989ccdc4dd755
Thorsten Schau <thorsten@codeho.org>
2022-03-22 14:24:46 +0100

    change plus to user image

--- shell/src/shared/molecules/Icons.svelte
+++ shell/src/shared/molecules/Icons.svelte
@@ -70,9 +70,13 @@ const icons = {
 </svg>
 
     `,
-  home: `<svg class="navbuttonicon" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M1 7.99988L10 0.999878L19 7.99988V18.9999C19 19.5303 18.7893 20.039 18.4142 20.4141C18.0391 20.7892 17.5304 20.9999 17 20.9999H3C2.46957 20.9999 1.96086 20.7892 1.58579 20.4141C1.21071 20.039 1 19.5303 1 18.9999V7.99988Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`,
+  home: `
+  <svg class="navbuttonicon" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.4 17H11.6C11.2686 17 11 16.7314 11 16.4V14.6C11 14.2686 11.2686 14 11.6 14H12.4C12.7314 14 13 14.2686 13 14.6V16.4C13 16.7314 12.7314 17 12.4 17Z" fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M3 9.5L12 4L21 9.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M19 13V19.4C19 19.7314 18.7314 20 18.4 20H5.6C5.26863 20 5 19.7314 5 19.4V13" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`,
   dash: `<svg
           class="navbuttonicon"
           viewBox="0 0 20 20"
--- shell/src/shared/molecules/NextNav/Components/ActionButton.svelte
+++ shell/src/shared/molecules/NextNav/Components/ActionButton.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 import { createEventDispatcher } from "svelte";
+import UserImage from "../../../atoms/UserImage.svelte";
+import { me } from "../../../stores/me";
 import Icons from "../../Icons.svelte";
 
 export let icon: string;
@@ -19,6 +21,14 @@ const dispatcher = createEventDispatcher();
       src="/logos/circles.png"
       alt="circlesLand"
       class="bg-white rounded-full navlogo" />
+  {:else if $me}
+    <div class="navlogo">
+      <UserImage
+        profile="{$me}"
+        size="{15}"
+        gradientRing="{true}"
+        profileLink="{false}" />
+    </div>
   {:else}
     <img
       src="/logos/actionbutton.svg"
