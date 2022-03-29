05f25e434ac0f7a1d4b29caf19d7ec1affcc7b5d
Thorsten Schau <letsgo@codeho.net>
2022-03-29 11:11:36 +0200

    move logout behind switcher

--- shell/src/dapps/o-passport.manifest.ts
+++ shell/src/dapps/o-passport.manifest.ts
@@ -155,7 +155,7 @@ export const passport: DappManifest<DappState> = {
         };
       });
 
-      return [...jumplistitems, ...profileItems];
+      return [...profileItems, ...jumplistitems];
     },
   },
   isEnabled: true,
