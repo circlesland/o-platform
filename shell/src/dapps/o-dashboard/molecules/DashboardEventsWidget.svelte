210d445e92260246c398c16d6fa2add054506e57
jaensen <daniel@circles.name>
2022-03-30 19:42:01 +0200

    use the stats from the api in the dashboard header

--- shell/src/dapps/o-dashboard/molecules/DashboardEventsWidget.svelte
+++ shell/src/dapps/o-dashboard/molecules/DashboardEventsWidget.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
 import CitizensProgressBar from "../atoms/CitizensProgressBar.svelte";
 import { push } from "svelte-spa-router";
+import {onMount} from "svelte";
+import {ApiClient} from "../../../shared/apiConnection";
+import {
+  Profile,
+  ProfilesDocument,
+  ProfilesQueryVariables,
+  Stats, StatsDocument,
+  StatsQueryVariables
+} from "../../../shared/api/data/types";
 
-export let profilesCount: number;
+export let stats: Stats = {
+  profilesCount: 0,
+  verificationsCount: 0,
+  leaderboard: [],
+  goals: {
+    currentValue: 0,
+    lastGoal: 0,
+    nextGoal: 0
+  }
+};
+
+onMount(async () => {
+  stats = await ApiClient.query<Stats, StatsQueryVariables>(StatsDocument,{});
+});
 </script>
 
 <section class="p-4 mb-4 bg-white rounded-lg shadow-md dashboard-card">
@@ -10,10 +32,12 @@ export let profilesCount: number;
     <h1>WANT MORE PARTIES?</h1>
     <span class="text-dark-lightest">Invite your friends.</span>
   </div>
-  <CitizensProgressBar
-    profilesCount="{profilesCount}"
-    previousTarget="{233}"
-    progressTarget="{377}" />
+  {#if stats.goals.currentValue > 0}
+    <CitizensProgressBar
+      profilesCount="{stats.goals.currentValue}"
+      previousTarget="{stats.goals.lastGoal}"
+      progressTarget="{stats.goals.nextGoal}" />
+  {/if}
   <div class="mt-4 mb-1 text-center">
     <button class="btn btn-primary" on:click="{() => push('/home/invites')}"
       >My Invites</button>
