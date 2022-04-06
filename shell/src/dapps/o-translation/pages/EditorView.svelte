bb01403d04e9a246040fc9f53c524c28c3272158
Ingo Collatz <incollatz@mail.de>
2022-04-06 11:10:21 +0200

    first i18n editor layout (in progress)

---
+++ shell/src/dapps/o-translation/pages/EditorView.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+
+</script>
+
+<section class="flex flex-col items-center justify-center p-6">
+    <div class="w-full text-center" style="background-color: blue;">
+        <div class="flex flex-row">
+            
+            <input type="text" value="Key">
+            <input type="text" value="String">
+        </div>
+    </div>
+
+    <div class="w-full text-center" style="background-color: blue;"><h1>Hallo</h1></div>
+</section>
