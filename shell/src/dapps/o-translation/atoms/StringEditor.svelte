bc248669aeae7c5327275fa649eca959d6a07f21
Ingo Collatz <incollatz@mail.de>
2022-04-14 05:45:31 +0200

    created save-function

--- shell/src/dapps/o-translation/atoms/StringEditor.svelte
+++ shell/src/dapps/o-translation/atoms/StringEditor.svelte
@@ -1,27 +1,44 @@
 <script lang="ts">
+
+import { I18n, MutationUpdateValueArgs, UpdateValueDocument } from "../../../shared/api/data/types";
+import { ApiClient } from "../../../shared/apiConnection";
+
 export let data = [];
 
+let inputValue: string;
+
+
 $: {
-    data;
+  data;
 }
 
-
+async function writeValueToDb(value: string, lang: string, key: string) {
+  await ApiClient.query<I18n, MutationUpdateValueArgs>(UpdateValueDocument, {
+    lang: lang,
+    key: key,
+    value: value,
+  });
+	window.location.reload();
+}
 </script>
-<div class="table">
-    <div class="table-header-group">
-        <div class="table-cell p-1">String</div>
-        <div class="table-cell p-1">Key</div>
-        <div class="table-cell p-1">Language</div>
-        <div class="table-cell p-1">Input</div>
-    </div>
-    {#each data as entry}
-    <div class="table-row-group">
-        <div class="table-cell break-all w-64 p-1">{entry.value}</div>
-        <div class="table-cell break-all p-1">{entry.key}</div>
-        <div class="table-cell p-1">{entry.lang}</div>
-        <div class="table-cell p-1"><input class="input" type="text" placeholder={entry.value}></div>
-        <button>Save</button>
-    </div>
-    {/each}
-</div>
 
+<div class="table">
+  <div class="table-header-group">
+    <div class="table-cell p-1">String</div>
+    <div class="table-cell p-1">Key</div>
+    <div class="table-cell p-1">Language</div>
+    <!--<div class="table-cell p-1">Version</div>-->
+    <div class="table-cell p-1">Input</div>
+  </div>
+  {#each data as entry}
+    <div class="table-row-group">
+      <div class="table-cell break-all w-64 p-1">{entry.value}</div>
+      <div class="table-cell break-all p-1">{entry.key}</div>
+      <div class="table-cell p-1">{entry.lang}</div>
+      <!--<div class="table-cell p-1">{entry.version}</div>-->
+      <form action="" on:submit="{() => writeValueToDb(inputValue, entry.lang, entry.key)}">
+        <div class="table-cell p-1"><input bind:value="{inputValue}" class="input" type="text" placeholder="{entry.value}" /></div>
+      </form>
+    </div>
+  {/each}
+</div>
--- shell/src/dapps/o-translation/pages/EditorView.svelte
+++ shell/src/dapps/o-translation/pages/EditorView.svelte
@@ -1,70 +1,95 @@
 <script lang="ts">
-import { isRequiredArgument } from "graphql";
+import { onMount } from "svelte";
 
-import {
-    onMount
-} from "svelte";
+import { GetAllStringsDocument, GetAllStringsQuery, I18n } from "../../../shared/api/data/types";
 
-
-import {
-    GetAllStringsDocument,
-    GetAllStringsQuery,
-    I18n
-} from "../../../shared/api/data/types";
-
-import {
-    ApiClient
-} from "../../../shared/apiConnection";
+import { ApiClient } from "../../../shared/apiConnection";
 import StringEditor from "../atoms/StringEditor.svelte";
+import { Environment } from "../../../shared/environment";
 
 let key: string;
 let value: string;
 let i18nData: I18n[];
+let allLanguages: string[] = [];
+let selectedLanguage: string = Environment.userLanguage;
 
-onMount(
-    async () => {
-        i18nData = await ApiClient.query < I18n[], GetAllStringsQuery > (GetAllStringsDocument, {});
-        console.log(i18nData)
+async function reload(selectedLanguage: string) {
+  const queryResult = await ApiClient.query<I18n[], GetAllStringsQuery>(GetAllStringsDocument, {});
 
-        i18nData.sort((a, b) => {
-            if (a.key < b.key) {
-                return -1;
-            }
-            if (a.key > b.key) {
-                return 1;
-            }
-            return 0;
-        })
-    }
-)
+  const allLanguageKeysInQueryResult = queryResult.toLookup((o) => o.lang);
+  allLanguages = Object.keys(allLanguageKeysInQueryResult);
 
+  i18nData = queryResult
+    .filter((o) => o.lang == selectedLanguage)
+    .sort((a, b) => {
+      if (a.key < b.key) {
+        return -1;
+      }
+      if (a.key > b.key) {
+        return 1;
+      }
+      return 0;
+    });
+}
+
+$: {
+  reload(selectedLanguage);
+}
+
+onMount(async () => {
+  reload(selectedLanguage);
+  const queryResult = await ApiClient.query<I18n[], GetAllStringsQuery>(GetAllStringsDocument, {});
+
+  const allLanguageKeysInQueryResult = queryResult.toLookup((o) => o.lang);
+  allLanguages = Object.keys(allLanguageKeysInQueryResult);
+
+  i18nData = queryResult
+    .filter((o) => o.lang == selectedLanguage)
+    .sort((a, b) => {
+      if (a.key < b.key) {
+        return -1;
+      }
+      if (a.key > b.key) {
+        return 1;
+      }
+      return 0;
+    });
+
+  console.log("i18nData", i18nData);
+  console.log("allLanguages", allLanguages);
+});
 
 const stringSubmitHandler = (event) => {
-    event.preventDefault();
-    i18nData = i18nData.filter(item => item.value.indexOf(value) !== -1)
-    console.log(i18nData)
-}
+  event.preventDefault();
+  i18nData = i18nData.filter((item) => item.value.startsWith(value));
+  console.log(i18nData);
+};
 
 const keySubmitHandler = (event) => {
-    event.preventDefault();
-    i18nData = i18nData.filter(item => item.key.startsWith(key))
-    console.log(i18nData)
-}
+  event.preventDefault();
+  i18nData = i18nData.filter((item) => item.key.startsWith(key));
+  console.log(i18nData);
+};
 </script>
 
 <section class="flex flex-col items-center justify-center p-6">
-    <div class="w-full flex flex-row flex-wrap items-stretch">
+  <div class="w-full flex flex-row flex-wrap items-stretch">
+    <form on:submit="{stringSubmitHandler}" action="">
+      <input bind:value class="m-1" type="text" placeholder="String" />
+    </form>
+    <form on:submit="{keySubmitHandler}" class="">
+      <input bind:value="{key}" class="m-1" type="text" placeholder="dapps.o-banking..." />
+    </form>
+    <form on:submit="{stringSubmitHandler}" action="">
+      <select bind:value="{selectedLanguage}" class="m-1">
+        {#each allLanguages as lang}
+          <option>{lang}</option>
+        {/each}
+      </select>
+    </form>
+  </div>
 
-        <form on:change={keySubmitHandler}  class="">
-            <input bind:value={key} class="m-1 input" type="text" placeholder="dapps.o-banking...">
-        </form>
-        <form on:change={stringSubmitHandler} action="">
-            <input bind:value={value} class="m-1 input" type="text" placeholder="String">
-        </form>
-
-    </div>
-
-    <div class="w-full text-center">
-        <StringEditor data={i18nData}/>
-    </div>
+  <div class="w-full text-center">
+    <StringEditor data="{i18nData}" />
+  </div>
 </section>
