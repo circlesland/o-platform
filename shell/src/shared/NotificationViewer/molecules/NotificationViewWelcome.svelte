91559d50aa3ae53cd85cab620e9813487dc81e19
jaensen <daniel@circles.name>
2022-05-18 23:40:35 +0200

    remove the info step from "upsertIdentity" and add the contents to NotificationViewWelcome instead

--- shell/src/shared/NotificationViewer/molecules/NotificationViewWelcome.svelte
+++ shell/src/shared/NotificationViewer/molecules/NotificationViewWelcome.svelte
@@ -5,8 +5,12 @@
 <div class="flex flex-col items-center space-y-4">
   <div
     class="flex flex-col items-center self-center w-full m-auto text-center justify-self-center ">
-    <div class="mt-4 text-3xl font-heading">{$_("shared.molecules.notificationViewer.molecules.notificationViewWelcome.welcomeToCirclesLand")}</div>
 
+    <div class="mt-4 text-3xl font-heading">{$_("shared.molecules.notificationViewer.molecules.notificationViewWelcome.welcomeToCirclesLand")}</div>
     <div class="mt-1 text-sm text-dark-lightest">{$_("shared.molecules.notificationViewer.molecules.notificationViewWelcome.duSchoenerMensch")}</div>
+
+    <div class="mt-4 text-3xl font-heading">{$_("dapps.o-passport.processes.upsertIdentity.editorContent.info.title")}</div>
+    <div class="mt-1 text-sm text-dark-lightest">{$_("dapps.o-passport.processes.upsertIdentity.editorContent.info.description")}</div>
+
   </div>
 </div>
