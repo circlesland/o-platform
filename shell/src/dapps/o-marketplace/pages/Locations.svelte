f9203de71a5092c7fed28ea94e583c56438e4890
Thorsten Schau <letsgo@codeho.net>
2022-06-07 13:37:53 +0200

    Pull shop infos  from shop instead of orga

--- shell/src/dapps/o-marketplace/pages/Locations.svelte
+++ shell/src/dapps/o-marketplace/pages/Locations.svelte
@@ -5,13 +5,13 @@ import { RuntimeDapp } from "@o-platform/o-interfaces/dist/runtimeDapp";
 import { Routable } from "@o-platform/o-interfaces/dist/routable";
 import { contacts } from "../../../shared/stores/contacts";
 import { ApiClient } from "../../../shared/apiConnection";
-import {CapabilityType, Organisation, Shop, ShopsDocument, ShopsQueryVariables} from "../../../shared/api/data/types";
+import { CapabilityType, Organisation, Shop, ShopsDocument, ShopsQueryVariables } from "../../../shared/api/data/types";
 import { onMount } from "svelte";
 import { trustFromContactMetadata } from "../../../shared/functions/trustFromContactMetadata";
 import { inbox } from "../../../shared/stores/inbox";
-import {getTrustedByShop} from "../processes/getTrustedByShop";
-import {me} from "../../../shared/stores/me";
-import {getTrusted} from "../processes/getTrusted";
+import { getTrustedByShop } from "../processes/getTrustedByShop";
+import { me } from "../../../shared/stores/me";
+import { getTrusted } from "../processes/getTrusted";
 
 export let runtimeDapp: RuntimeDapp<any>;
 export let routable: Routable;
@@ -36,6 +36,7 @@ async function load() {
       const { trustIn, trustOut } = trustFromContactMetadata(contact);
       return {
         shopId: o.id,
+        shop: o,
         orga: {
           ...o.owner,
           largeBannerUrl: o.largeBannerUrl,
@@ -51,7 +52,9 @@ async function load() {
 onMount(async () => await load());
 
 async function locationClicked(orga) {
-  const alreadyVerified = (await me.getSessionInfo()).capabilities.find(o => o.type == CapabilityType.VerifiedByHumanode);
+  const alreadyVerified = (await me.getSessionInfo()).capabilities.find(
+    (o) => o.type == CapabilityType.VerifiedByHumanode
+  );
   if (orga.enabled) {
     loadLocationPage(`${orga.productListingType}/${orga.shopId}`);
   } else if (!alreadyVerified) {
@@ -74,22 +77,19 @@ async function locationClicked(orga) {
     {#each orgas as orga}
       <section
         class="flex items-start m-4 rounded-xl"
-        class:cursor-pointer="{orga.enabled}"
+        class:cursor-pointer="{orga.shop.enabled}"
         on:click="{() => locationClicked(orga)}">
         <div class="flex flex-col w-full ">
           <header class=" rounded-xl headerImageContainer">
             <div class="relative rounded-xl image-wrapper">
-              <img
-                src="{orga.orga.largeBannerUrl}"
-                alt=""
-                class="w-full rounded-xl" />
+              <img src="{orga.shop.largeBannerUrl}" alt="" class="w-full rounded-xl" />
               <div
                 class="absolute right-0 py-2 pt-3 pl-4 pr-2 mt-2 text-3xl rounded-l-full font-heading top-2 bg-light-lightest">
-                <span class="inline-block">{orga.orga.name}</span>
+                <span class="inline-block">{orga.shop.name}</span>
               </div>
               <div
                 class="absolute right-0 py-2 pl-4 pr-1 mt-2 text-xs rounded-l-full cursor-pointer bottom-4 bg-alert-lightest has-tooltip"
-                class:hidden="{orga.enabled}">
+                class:hidden="{orga.shop.enabled}">
                 <span class="px-2 mt-12 text-sm bg-white rounded shadow-sm right-20 tooltip bottom-10">
                   <!--Find one of our friendly circlesland people to trust you for
                   this shop.-->
