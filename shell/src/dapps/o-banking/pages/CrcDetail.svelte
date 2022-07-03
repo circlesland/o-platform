8b04aaea258d61a5d97289a06fd8f9ba1848b35a
jaensen <daniel@circles.name>
2022-07-03 13:07:35 +0200

    change "$_" to <Label> in CrcDetail.svelte

--- shell/src/dapps/o-banking/pages/CrcDetail.svelte
+++ shell/src/dapps/o-banking/pages/CrcDetail.svelte
@@ -13,6 +13,7 @@ import ItemCard from "../../../shared/atoms/ItemCard.svelte";
 import Card from "../../../shared/atoms/Card.svelte";
 import ItemCard from "../../../shared/atoms/ItemCard.svelte";
 
+
 let loading = true;
 let balances: AssetBalance[] = [];
 
