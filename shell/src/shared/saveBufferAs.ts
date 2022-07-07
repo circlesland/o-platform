dc725752e270f11d98f8d7f2332fe0c1adc9d1dc
Thorsten Schau <letsgo@codeho.net>
2022-07-07 18:10:34 +0200

    Fix PDF Download

--- shell/src/shared/saveBufferAs.ts
+++ shell/src/shared/saveBufferAs.ts
@@ -1,6 +1,12 @@
-export function saveBufferAs(data:Buffer, fileName:string) {
-  let file = new File([data], fileName, {type: "application/pdf"});
+export function saveBufferAs(data: Buffer, fileName: string) {
+  let file = new File([data], fileName, { type: "application/pdf" });
   let exportUrl = URL.createObjectURL(file);
-  window.location.assign(exportUrl);
+  let file_path = exportUrl;
+  let a: HTMLAnchorElement = document.createElement("A") as HTMLAnchorElement;
+  a.href = file_path;
+  a.download = file_path.substr(file_path.lastIndexOf("/") + 1);
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
   URL.revokeObjectURL(exportUrl);
 }
