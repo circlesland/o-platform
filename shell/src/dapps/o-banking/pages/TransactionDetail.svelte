be453358cac104ee692b89c672f952258a2034cc
jaensen <daniel@circles.name>
2022-07-19 20:01:28 +0200

    replaced nearly all calls to $_() from svelte-i18n with <Label> components

--- shell/src/dapps/o-banking/pages/TransactionDetail.svelte
+++ shell/src/dapps/o-banking/pages/TransactionDetail.svelte
@@ -7,20 +7,20 @@
   import {me} from "../../../shared/stores/me";
   import {Currency} from "../../../shared/currency";
 
-  import {
-    CrcHubTransfer,
-    CrcMinting,
-    Erc20Transfer,
-    EventType,
-    Profile,
-    ProfileEvent,
-  } from "../../../shared/api/data/types";
+    import {
+      CrcHubTransfer,
+      CrcMinting,
+      Erc20Transfer,
+      EventType,
+      Profile,
+      ProfileEvent,
+    } from "../../../shared/api/data/types";
 
-import { _ } from "svelte-i18n";
-import { myTransactions } from "../../../shared/stores/myTransactions";
-import Icons from "../../../shared/molecules/Icons.svelte";
+  import Label from "../../../shared/atoms/Label.svelte";
+  import {myTransactions} from "../../../shared/stores/myTransactions";
+  import Icons from "../../../shared/molecules/Icons.svelte";
 
-  export let transactionHash: string;
+    export let transactionHash: string;
 
 let transfer: ProfileEvent;
 let classes: string;
@@ -50,8 +50,14 @@ onMount(async () => {
     )
   }
   if (!transfer) {
+    transfer = await myTransactions.findByPrimaryKey(
+      EventType.Erc20Transfer,
+      transactionHash
+    )
+  }
+  if (!transfer) {
     transfer = await myTransactions.findSingleItemFallback(
-      [EventType.CrcHubTransfer, EventType.CrcMinting, EventType.Erc20Transfer],
+      [EventType.CrcHubTransfer, EventType.CrcMinting, EventType.Erc20Transfer, EventType.Erc20Transfer],
       transactionHash
     );
   }
@@ -194,12 +200,12 @@ function openDetail(transfer: ProfileEvent) {
         }}">
         {#if transfer.direction === "in"}
           <span class="mt-4 text-xl break-words">
-            {$_("dapps.o-banking.pages.transactionDetail.from" )}
+            <Label key="dapps.o-banking.pages.transactionDetail.from" />
             {displayableName ? displayableName : ""}
           </span>
         {:else}
           <span class="mt-4 text-xl break-words">
-            {$_("dapps.o-banking.pages.transactionDetail.to" )}
+            <Label key="dapps.o-banking.pages.transactionDetail.to" />
             {displayableName ? displayableName : ""}
           </span>
         {/if}
@@ -210,7 +216,7 @@ function openDetail(transfer: ProfileEvent) {
       <!-- {#if path && path.transfers}
         <div class="flex flex-col w-full space-y-1">
           <div class="mb-1 text-left text-2xs text-dark-lightest">
-            {$_("dapps.o-banking.pages.transactionDetail.paymentPath" )}
+            <Label key="dapps.o-banking.pages.transactionDetail.paymentPath" />
           </div>
           <div class="flex items-center w-full">
             <CirclesTransferGraph
@@ -222,7 +228,7 @@ function openDetail(transfer: ProfileEvent) {
       {/if} -->
       <div class="flex flex-col w-full space-y-1">
         <div class="mb-1 text-left text-2xs text-dark-lightest">
-          {$_("common.date" )}
+          <Label key="common.date" />
         </div>
         <div class="flex items-center w-full">
           <div class="text-left ">
@@ -234,7 +240,7 @@ function openDetail(transfer: ProfileEvent) {
       </div>
       <div class="flex flex-col w-full space-y-1">
         <div class="mb-1 text-left text-2xs text-dark-lightest">
-          {$_("dapps.o-banking.pages.transactionDetail.fullAmountCrc" )}
+          <Label key="dapps.o-banking.pages.transactionDetail.fullAmountCrc" />
         </div>
         <div class="flex items-center w-full">
           <div class="text-left ">
@@ -255,7 +261,7 @@ function openDetail(transfer: ProfileEvent) {
       </div>
       <!-- <div class="flex flex-col w-full space-y-1">
         <div class="mb-1 text-left text-2xs text-dark-lightest">
-          {$_("dapps.o-banking.pages.transactionDetail.amountCircles" )}
+          <Label key="dapps.o-banking.pages.transactionDetail.amountCircles" />
         </div>
         <div class="flex items-center w-full">
           <div class="text-left ">
@@ -269,7 +275,7 @@ function openDetail(transfer: ProfileEvent) {
       </div> -->
       <div class="flex flex-col w-full space-y-1">
         <div class="mb-1 text-left text-2xs text-dark-lightest">
-          {$_("common.from" )}
+          <Label key="common.from" />
         </div>
         <div class="flex items-center w-full">
           <div class="text-left break-all">{fromProfile.circlesAddress}</div>
@@ -277,7 +283,7 @@ function openDetail(transfer: ProfileEvent) {
       </div>
       <div class="flex flex-col w-full space-y-1">
         <div class="mb-1 text-left text-2xs text-dark-lightest">
-          {$_("common.to" )}
+          <Label key="common.to" />
         </div>
         <div class="flex items-center w-full">
           <div class="text-left break-all">{toProfile.circlesAddress}</div>
@@ -285,7 +291,7 @@ function openDetail(transfer: ProfileEvent) {
       </div>
       <div class="flex flex-col w-full space-y-1">
         <div class="mb-1 text-left text-2xs text-dark-lightest">
-          {$_("common.block" )}
+          <Label key="common.block" />
         </div>
         <div class="flex items-center w-full">
           <div class="text-left break-all">{transfer.block_number}</div>
@@ -293,7 +299,7 @@ function openDetail(transfer: ProfileEvent) {
       </div>
       <div class="flex flex-col w-full space-y-1">
         <div class="mb-1 text-left text-2xs text-dark-lightest">
-          {$_("dapps.o-banking.pages.transactionDetail.transactionHash" )}
+          <Label key="dapps.o-banking.pages.transactionDetail.transactionHash" />
         </div>
         <div class="flex items-center w-full text-primarydark">
           <div class="text-left break-all">
