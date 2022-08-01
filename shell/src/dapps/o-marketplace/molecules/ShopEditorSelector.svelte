6bf8480cfb5f8a00db3dfaa618a4575dde9e1c3f
Thorsten Schau <letsgo@codeho.net>
2022-08-01 12:19:43 +0200

    shopeditor selector (unfinsihed)

---
+++ shell/src/dapps/o-marketplace/molecules/ShopEditorSelector.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { createEventDispatcher } from "svelte";
+const dispatch = createEventDispatcher();
+
+export let shopIndex;
+export let shops;
+
+const selectShop = () => {
+  dispatch("indexChange", shopIndex);
+  localStorage.setItem("editShopIndex", shopIndex);
+};
+</script>
+
+<div class="flex flex-col justify-center mb-20 space-y-4">
+  <select class="self-center max-w-xs select" bind:value="{shopIndex}" on:change="{selectShop}">
+    <option disabled selected>Select a Shop</option>
+
+    {#each shops as shop, i}
+      <option value="{i}">Shop: {shop.name}</option>
+    {/each}
+  </select>
+</div>
