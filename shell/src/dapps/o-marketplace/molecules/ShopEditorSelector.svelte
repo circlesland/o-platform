e6100c0f94a2e856ff5332c138cee42d8261d30c
Thorsten Schau <letsgo@codeho.net>
2022-08-01 12:27:57 +0200

    Fix selected in ShopEditor dropdown

--- shell/src/dapps/o-marketplace/molecules/ShopEditorSelector.svelte
+++ shell/src/dapps/o-marketplace/molecules/ShopEditorSelector.svelte
@@ -1,22 +1,31 @@
 <script lang="ts">
 import { createEventDispatcher } from "svelte";
+import { onMount } from "svelte";
+
 const dispatch = createEventDispatcher();
 
 export let shopIndex;
 export let shops;
+let selectedIndex = 0;
+
+onMount(() => {
+  selectedIndex = parseInt(shopIndex) || 0;
+});
 
 const selectShop = () => {
-  dispatch("indexChange", shopIndex);
-  localStorage.setItem("editShopIndex", shopIndex);
+  dispatch("indexChange", selectedIndex);
+  localStorage.setItem("editShopIndex", selectedIndex.toString());
 };
 </script>
 
-<div class="flex flex-col justify-center mb-20 space-y-4">
-  <select class="self-center max-w-xs select" bind:value="{shopIndex}" on:change="{selectShop}">
-    <option disabled selected>Select a Shop</option>
+{#if selectedIndex !== undefined}
+  <div class="flex flex-col justify-center mb-20 space-y-4">
+    <select class="self-center max-w-xs select" bind:value="{selectedIndex}" on:change="{selectShop}">
+      <option disabled selected>Select a Shop</option>
 
-    {#each shops as shop, i}
-      <option value="{i}">Shop: {shop.name}</option>
-    {/each}
-  </select>
-</div>
+      {#each shops as shop, i}
+        <option value="{i}">Shop: {shop.name}</option>
+      {/each}
+    </select>
+  </div>
+{/if}
