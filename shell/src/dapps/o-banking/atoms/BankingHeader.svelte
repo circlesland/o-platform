ec915f87023203260d49c3cc286940888ac13642
Thorsten Schau <letsgo@codeho.net>
2022-10-05 10:34:44 +0200

    language check

--- shell/src/dapps/o-banking/atoms/BankingHeader.svelte
+++ shell/src/dapps/o-banking/atoms/BankingHeader.svelte
@@ -4,7 +4,7 @@ import PageHeader from "src/shared/atoms/PageHeader.svelte";
 import { RuntimeDapp } from "@o-platform/o-interfaces/dist/runtimeDapp";
 import { Routable } from "@o-platform/o-interfaces/dist/routable";
 import { me } from "../../../shared/stores/me";
-import { _, getLocaleFromNavigator } from "svelte-i18n";
+
 import { Currency } from "../../../shared/currency";
 import { BN } from "ethereumjs-util";
 import { assetBalances } from "../../../shared/stores/assetsBalances";
@@ -37,7 +37,7 @@ $: {
         <div class="relative pt-2 text-center text-white">
           <span class="inline-block tracking-wide font-heading">
             <section class="m-4 -mb-4 text-center">
-              {#if getLocaleFromNavigator() !== "id"}
+              {#if Environment.userLanguage !== "id"}
                 <h1 class="text-5xl sm:text-7xl">
                   {balanceEuro}<span class=" font-enso">{Currency.currencySymbol["EURS"]}</span>
                 </h1>
@@ -53,30 +53,3 @@ $: {
     </span>
   </div>
 </PageHeader>
-
-<!-- <div class="w-4/5 blob bg-banking-light h-72" style="border-radius: 32% 1% 118% 46% / 99% 69% 103% 73%">
-    <div class="flex items-center h-full pt-2 pl-10 text-white">
-      <span class="inline-block tracking-wide text-7xl font-enso">
-        {balanceEuro} <span class="text-7xl font-enso">{Currency.currencySymbol["EURS"]}</span>
-        <br />
-        {balanceTime}<Icons icon="timeCircle" size="{16}" customClass="inline inline-icon" />
-      </span>
-
-      
-    {#if !$mySafe.ui.loadingText || $mySafe.ui.loadingText === ''}
-      <small class="block whitespace-nowrap">
-        {$mySafe.transfers && $mySafe.transfers.rows ? $mySafe.transfers.rows.length : ''}
-        transactions
-      </small>
-    {/if}
-      <small class="block whitespace-nowrap">
-        {$mySafe.ui.loadingPercent ? $mySafe.ui.loadingText : ''}
-      </small>
-    <progress
-      class="progress progress-accent transaction-update-progress"
-      value={$mySafe.ui.loadingPercent ? $mySafe.ui.loadingPercent : 0}
-      max="100"
-    />
-    
-    </div> 
-  </div>-->
